9f0b5fd3974a2a620b9250056852d327da268cbe
Gerry Shaw <gerry_shaw@users.sourceforge.net>
2001-08-10 01:56:15 +0000

    initial checkin

---
+++ examples/HelloWorld/HelloWorld.vb
@@ -0,0 +1,10 @@
+imports System
+	public class MainApp
+
+	    shared sub Main()
+ 	        Console.WriteLine("Hello World using VB.NET")
+                return
+    	    end sub
+
+	end class
+     
---
+++ examples/UserTask/UserTask.cs
@@ -0,0 +1,15 @@
+namespace OrganicBit.NAnt.SampleTask {
+
+	using OrganicBit.NAnt;
+
+	[TaskName("usertask")]
+	public class TestTask : Task {
+
+		[TaskAttribute("message", Required=true)]
+		string _message = null;
+
+		protected override void ExecuteTask() {
+			Log.WriteLine(LogPrefix + _message.ToUpper());
+		}
+	}
+}
