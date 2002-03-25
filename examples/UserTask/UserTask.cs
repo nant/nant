96e7aa670fb544833ae25ce68a698584918e5040
Gerry Shaw <gerry_shaw@users.sourceforge.net>
2002-03-25 16:19:42 +0000

    * updated to use new style of writing tasks

--- examples/UserTask/UserTask.cs
+++ examples/UserTask/UserTask.cs
@@ -1,13 +1,18 @@
-namespace SourceForge.NAnt.SampleTask {
+using SourceForge.NAnt;
+using SourceForge.NAnt.Attributes;
 
-    using SourceForge.NAnt;
-    using SourceForge.NAnt.Attributes;
+namespace SourceForge.NAnt.SampleTask {
 
     [TaskName("usertask")]
     public class TestTask : Task {
 
-        [TaskAttribute("message", Required=true)]
         string _message = null;
+
+        [TaskAttribute("message", Required=true)]
+        public string FileName {
+            get { return _message; }
+            set { _message = value; }
+        }
 
         protected override void ExecuteTask() {
             Log.WriteLine(LogPrefix + _message.ToUpper());
