984f645a9d033cb27504361163a8da1f9cbfd13d
drieseng <gert.driesen@telenet.be>
2003-04-22 06:49:39 +0000

    - fixed nullreferenceexception when arg element without value and file attribute was specified. - added if/unless attributes to arg element to allow arguments to be specified conditionally. - restructured code

--- src/NAnt.Core/Tasks/ExternalProgramBase.cs
+++ src/NAnt.Core/Tasks/ExternalProgramBase.cs
@@ -25,8 +25,8 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Text;
-using System.Xml;
 using System.Threading;
+using System.Xml;
 
 using SourceForge.NAnt.Attributes;
 
@@ -35,21 +35,48 @@ namespace SourceForge.NAnt.Tasks {
     /// Provides the abstract base class for tasks that execute external applications.
     /// </summary>
     public abstract class ExternalProgramBase : Task {
+        #region Private Instance Fields
+
+        private Hashtable _htThreadStream = new Hashtable();
+        private StringCollection _args = new StringCollection();       
+
+        #endregion Private Instance Fields
+
+        #region Private Static Fields
+
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        /// <summary>Gets the application to start.</summary>
+        #endregion Private Static Fields
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// Gets the application to start.
+        /// </summary>
         public abstract string ProgramFileName { get; }
 
-        /// <summary>Gets the command line arguments for the application.</summary>
+        /// <summary>
+        /// Gets the command line arguments for the application.
+        /// </summary>
         public abstract string ProgramArguments { get; }
 
-        /// <summary>The file to which the standard output will be redirected.</summary>
-        public virtual string OutputFile { get { return null; } }
+        /// <summary>
+        /// The file to which the standard output will be redirected.
+        /// </summary>
+        public virtual string OutputFile { 
+            get { return null; } 
+        }
         
-        /// <summary>true if the output file is to be appended to.</summary>
-        public virtual bool OutputAppend { get { return false; } }
+        /// <summary>
+        /// true if the output file is to be appended to.
+        /// </summary>
+        public virtual bool OutputAppend { 
+            get { return false; } 
+        }
       
-        /// <summary>Gets the working directory for the application.</summary>
+        /// <summary>
+        /// Gets the working directory for the application.
+        /// </summary>
         public virtual string BaseDirectory { 
             get {
                 if (Project != null) {
@@ -60,46 +87,130 @@ namespace SourceForge.NAnt.Tasks {
             }
             set{} //so that it can be overriden.
         }
+
         /// <summary>
-        /// default to task name but can be overridden by derived classes
+        /// Defaults to task name but can be overridden by derived classes.
         /// </summary>
         public virtual string ExeName {
             get { return Name; }
         }
         
         /// <summary>
-        /// Override in a derived class to 
+        /// Gets a value indicating whether the external program should be executed
+        /// using a runtime engine, if configured.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The default implementation will always execute external programs without
+        /// using a runtime engine.
+        /// </para>
+        /// <para>
+        /// Derived classes should override this property to change this behaviour.
+        /// </para>
+        /// </remarks>
         protected virtual bool UsesRuntimeEngine {
             get { return false; }
         }
-        /// <summary>The maximum amount of time the application is allowed to execute, expressed in milliseconds.  Defaults to no time-out.</summary>
-        public virtual int TimeOut { get { return Int32.MaxValue;  } set{} }
-
-        private Hashtable _htThreadStream = new Hashtable();
-
-        StringCollection _args = new StringCollection();       
-
-        /// <summary>Get the command line arguments for the application.</summary>
-        protected StringCollection Args {
-            get { return _args; }
+        /// <summary>
+        /// The maximum amount of time the application is allowed to execute, 
+        /// expressed in milliseconds.  Defaults to no time-out.
+        /// </summary>
+        public virtual int TimeOut { 
+            get { return Int32.MaxValue; } 
+            set{} 
         }
-        
+
         [BuildElementArray("arg")]
-        public ArgElement[] ArgElements{
+        public ArgElement[] ArgElements {
             set {
                 ArgElement[] args = value as ArgElement[];
-                foreach(ArgElement arg in args){
-                    _args.Add(arg.File==null ? arg.Value : arg.File);
+                foreach(ArgElement arg in args) {
+                    if (arg.IfDefined && !arg.UnlessDefined) {
+                        if (arg.Value != null || arg.File != null) {
+                            _args.Add(arg.File == null ? arg.Value : arg.File);
+                        } else {
+                            Log.WriteLine(
+                                string.Format(CultureInfo.InvariantCulture, 
+                                "{0} skipped arg element without value and file attribute.",
+                                Location));
+                        }
+                    }
                 }
             }
         }
 
-        /// <summary>Get the command line arguments, separated by spaces.</summary>
+        #endregion Public Instance Properties
+
+        #region Protected Instance Properties
+
+        /// <summary>
+        /// Get the command line arguments for the application.
+        /// </summary>
+        protected StringCollection Args {
+            get { return _args; }
+        }
+
+        #endregion Protected Instance Properties
+
+        #region Override implementation of Task
+
+        protected override void ExecuteTask() {
+            try {
+                // Start the external process
+                Process process = StartProcess();
+                Thread outputThread = new Thread( new ThreadStart( StreamReaderThread_Output ) );
+                outputThread.Name = "Output";
+                Thread errorThread = new Thread( new ThreadStart( StreamReaderThread_Error ) );
+                errorThread.Name = "Error";
+                _htThreadStream[ outputThread.Name ] = process.StandardOutput;
+                _htThreadStream[ errorThread.Name ] = process.StandardError;
+
+                outputThread.Start();
+                errorThread.Start();
+
+                // Wait for the process to terminate
+                process.WaitForExit(TimeOut);
+                // Wait for the threads to terminate
+                outputThread.Join();
+                errorThread.Join();
+                _htThreadStream.Clear();
+
+                if (process.ExitCode != 0){
+                    throw new BuildException(
+                        String.Format(CultureInfo.InvariantCulture, 
+                        "External Program Failed: {0} (return code was {1})", 
+                        ProgramFileName, 
+                        process.ExitCode), 
+                        Location);
+                }
+            } catch (BuildException e) {
+                if (FailOnError) {
+                    throw;
+                } else {
+                    logger.Error("Execution Error", e);
+                    Log.WriteLine(e.Message);
+                }
+            } catch (Exception e) {
+                logger.Error("Execution Error", e);
+                
+                throw new BuildException(
+                    String.Format(CultureInfo.InvariantCulture, "{0}: {1} had errors. Please see log4net log.", GetType().ToString(), ProgramFileName), 
+                    Location, 
+                    e);
+            }
+        }
+
+        #endregion Override implementation of Task
+
+        #region Public Instance Methods
+
+        /// <summary>
+        /// Gets the command line arguments, separated by spaces.
+        /// </summary>
         public string GetCommandLine() {
             // append any nested <arg> arguments to command line
             StringBuilder arguments = new StringBuilder(ProgramArguments);
-            foreach (string arg in _args) {
+            foreach (string arg in Args) {
                 arguments.Append(' ');
                 //if the arg contains a space, but isn't quoted, quote it.
                 if(arg.IndexOf(" ") > 0 && !(arg.StartsWith("\"") && arg.EndsWith("\""))) {
@@ -114,7 +225,10 @@ namespace SourceForge.NAnt.Tasks {
             return arguments.ToString();
         }
 
-        
+        #endregion Public Instance Methods
+
+        #region Public Instance Methods
+
         /// <summary>
         /// Sets the StartInfo Options and returns a new Process that can be run.
         /// </summary>
@@ -159,7 +273,11 @@ namespace SourceForge.NAnt.Tasks {
             return p;
         }
 
-        /// <summary>        /// Read from the stream until the external program is ended        /// </summary>
+        #endregion Public Instance Methods
+
+        #region Private Instance Methods
+
+        /// <summary>        /// Reads from the stream until the external program is ended.        /// </summary>
         private void StreamReaderThread_Output() {
             StreamReader reader = ( StreamReader )_htThreadStream[ Thread.CurrentThread.Name ];
             while ( true ) {                        
@@ -181,8 +299,7 @@ namespace SourceForge.NAnt.Tasks {
                 }
             }
         }
-
-        /// <summary>        /// Read from the stream until the external program is ended        /// </summary>
+        /// <summary>        /// Reads from the stream until the external program is ended.        /// </summary>
         private void StreamReaderThread_Error() {
             StreamReader reader = ( StreamReader )_htThreadStream[ Thread.CurrentThread.Name ];
             while ( true ) {                        
@@ -203,60 +320,23 @@ namespace SourceForge.NAnt.Tasks {
             }
         }
 
-        protected override void ExecuteTask() {
-            try {
-                // Start the external process
-                Process process = StartProcess();
-                Thread outputThread = new Thread( new ThreadStart( StreamReaderThread_Output ) );
-                outputThread.Name = "Output";
-                Thread errorThread = new Thread( new ThreadStart( StreamReaderThread_Error ) );
-                errorThread.Name = "Error";
-                _htThreadStream[ outputThread.Name ] = process.StandardOutput;
-                _htThreadStream[ errorThread.Name ] = process.StandardError;
-
-                outputThread.Start();
-                errorThread.Start();
-
-                // Wait for the process to terminate
-                process.WaitForExit(TimeOut);
-                // Wait for the threads to terminate
-                outputThread.Join();
-                errorThread.Join();
-                _htThreadStream.Clear();
-
-                if (process.ExitCode != 0){
-                    throw new BuildException(
-                                String.Format(CultureInfo.InvariantCulture, 
-                                    "External Program Failed: {0} (return code was {1})", 
-                                    ProgramFileName, 
-                                    process.ExitCode), 
-                                Location);
-                }
-            } catch (BuildException e) {
-                if (FailOnError) {
-                    throw;
-                } else {
-                    logger.Error("Execution Error", e);
-                    Log.WriteLine(e.Message);
-                }
-            } catch (Exception e) {
-                logger.Error("Execution Error", e);
-                
-                throw new BuildException(
-                    String.Format(CultureInfo.InvariantCulture, "{0}: {1} had errors. Please see log4net log.", GetType().ToString(), ProgramFileName), 
-                    Location, 
-                    e);
-            }
-        }
+        #endregion Private Instance Methods
     }
     
-    
     public class ArgElement : Element {
+        #region Private Instance Fields
+
         private string _value = null;
         private string _file = null;
+        private bool _ifDefined = true;
+        private bool _unlessDefined = false;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
 
         /// <summary>
-        /// Value of this property. Default is null;
+        /// Value of this argument.
         /// </summary>
         [TaskAttribute("value")]
         public string Value {
@@ -265,12 +345,38 @@ namespace SourceForge.NAnt.Tasks {
         }
 
         /// <summary>
-        /// File of this property. Default is null;
+        /// File of this argument.
         /// </summary>
         [TaskAttribute("file")]
         public string File {
             get { return _file; }
             set { _file = Project.GetFullPath(value); }
         }
+
+        /// <summary>
+        /// Indicates if the argument should be passed to the external program. 
+        /// If true then the argument will be passed; otherwise skipped. 
+        /// Default is "true".
+        /// </summary>
+        [TaskAttribute("if")]
+        [BooleanValidator()]
+        public bool IfDefined {
+            get { return _ifDefined; }
+            set { _ifDefined = value; }
+        }
+
+        /// <summary>
+        /// Indicates if the argument should not be passed to the external program. 
+        /// If false then the argument will be passed; otherwise skipped. 
+        /// Default is "false".
+        /// </summary>
+        [TaskAttribute("unless")]
+        [BooleanValidator()]
+        public bool UnlessDefined {
+            get { return _unlessDefined; }
+            set { _unlessDefined = value; }
+        }
+
+        #endregion Public Instance Properties
     }
 }
