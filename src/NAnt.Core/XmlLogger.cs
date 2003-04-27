425ddd3f31eb82145bd7b0efe3a16160d69f96de
drieseng <gert.driesen@telenet.be>
2003-04-27 17:23:49 +0000

    fixed fxcop issues and restructured code

--- src/NAnt.Core/XmlLogger.cs
+++ src/NAnt.Core/XmlLogger.cs
@@ -1,5 +1,5 @@
 // NAnt - A .NET build tool
-// Copyright (C) 2001-2002 Gerry Shaw
+// Copyright (C) 2001-2003 Gerry Shaw
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,64 +18,44 @@
 // William E. Caputo (wecaputo@thoughtworks.com | logosity@yahoo.com)
 
 using System;
-using System.Collections;
-using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
 
 namespace SourceForge.NAnt {
-    /// <summary>Used to wrap log messages in xml  &lt;message/&gt; elements</summary>
+    /// <summary>
+    /// Used to wrap log messages in xml &lt;message/&gt; elements.
+    /// </summary>
     public class XmlLogger : LogListener, IBuildEventConsumer {
-
-        public class Elements {
-            public const string BUILD_RESULTS = "buildresults";
-            public const string MESSAGE = "message";
-            public const string TARGET = "target";
-            public const string TASK = "task";
-            public const string STATUS = "status";
-        }
-
-        public class Attributes {
-            public const string PROJECT = "project";
-            public const string MESSAGETYPE = "type";
-        }
+        #region Private Instance Fields
 
         private TextWriter _writer = Console.Out;
         private XmlTextWriter _xmlWriter = new XmlTextWriter(Console.Out);
 
-        public XmlLogger() {
+        #endregion Private Instance Fields
 
+        #region Public Instance Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlLogger" /> class.
+        /// </summary>
+        public XmlLogger() {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlLogger" /> class
+        /// with the specified <see cref="TextWriter" />.
+        /// </summary>
+        /// <param name="writer">The <see cref="TextWriter" /> to which the build output should be written.</param>
         public XmlLogger(TextWriter writer) {
             _writer = writer;
             _xmlWriter = new XmlTextWriter(_writer);
             _xmlWriter.Formatting = Formatting.Indented;
         }
-        
-        public string StripFormatting(string message) {
-            //looking for zero or more white space from front of line followed by
-            //one or more of just about anything between [ and ] followed by a message
-            //which we will capture. '    [blah] 
-            Regex r = new Regex(@"(?ms)^\s*?\[[\s\w\d]+\](.+)");
 
-            Match m = r.Match(message);
-            if(m.Success) 
-            {
-                return m.Groups[1].Captures[0].Value.Trim();
-            }
-            return message;
-        }
+        #endregion Public Instance Constructors
 
-        public bool IsJustWhiteSpace(string message) {
-            Regex r = new Regex(@"^\s*$");
-
-            return r.Match(message).Success;
-        }
-
-        #region LogListener Overrides
+        #region Override implementation of LogListener
 
         public override void Write(string formattedMessage) {
             WriteLine(formattedMessage, null);
@@ -85,21 +65,20 @@ namespace SourceForge.NAnt {
             WriteLine(message, null);
         }
         
-        public override void WriteLine(string message, string messageType) 
-        {
+        public override void WriteLine(string message, string messageType) {
             string rawMessage = StripFormatting(message.Trim());
             if (IsJustWhiteSpace(rawMessage)) {
                 return;
             }
             
-            _xmlWriter.WriteStartElement(Elements.MESSAGE);
+            _xmlWriter.WriteStartElement(Elements.Message);
 
-            if (messageType != null && messageType != String.Empty) {
-                _xmlWriter.WriteAttributeString(Attributes.MESSAGETYPE, messageType);
+            if (messageType != null && messageType.Length != 0) {
+                _xmlWriter.WriteAttributeString(Attributes.MessageType, messageType);
             }
             
             if (IsValidXml(rawMessage)) {
-                rawMessage = Regex.Replace(rawMessage, @"<\?.*\?>", String.Empty);
+                rawMessage = Regex.Replace(rawMessage, @"<\?.*\?>", string.Empty);
                 _xmlWriter.WriteRaw(rawMessage);
             } else {
                 _xmlWriter.WriteCData(StripCData(rawMessage));
@@ -108,39 +87,28 @@ namespace SourceForge.NAnt {
             _xmlWriter.Flush();
         }
 
-        private bool IsValidXml(string message) {
-            if (Regex.Match(message, @"^<.*>").Success) {
-                // validate xml
-                XmlValidatingReader reader = new XmlValidatingReader(message, XmlNodeType.Element, null);
-                try { while (reader.Read()) { } }
-                catch (Exception) { return false; }
-                finally { reader.Close(); }
-                return true;
-            }
-            return false;
-        }
-        
-        private string StripCData(string message) {
-            string strippedMessage = Regex.Replace(message, @"<!\[CDATA\[", String.Empty);
-            return Regex.Replace(strippedMessage, @"\]\]>", String.Empty);
-        }
-        
         public override void Flush() {
             _writer.Flush();
         }
 
-        /// <summary>Returns the contents of log captured.</summary>
+        #endregion Override implementation of LogListener
+
+        #region Override implementation of Object
+
+        /// <summary>
+        /// Returns the contents of log captured.
+        /// </summary>
         public override string ToString() {
             return _writer.ToString();
         }
 
-        #endregion
+        #endregion Override implementation of Object
 
-        #region IBuildEventConsumer Implementation
+        #region Implementation of IBuildEventConsumer
 
         public void BuildStarted(object obj, BuildEventArgs args) {
-            _xmlWriter.WriteStartElement(Elements.BUILD_RESULTS);
-            _xmlWriter.WriteAttributeString(Attributes.PROJECT, args.Name);
+            _xmlWriter.WriteStartElement(Elements.BuildResults);
+            _xmlWriter.WriteAttributeString(Attributes.Project, args.Name);
         }
 
         public void BuildFinished(object obj, BuildEventArgs args) {
@@ -148,7 +116,7 @@ namespace SourceForge.NAnt {
         }
 
         public void TargetStarted(object obj, BuildEventArgs args) {
-            _xmlWriter.WriteStartElement(Elements.TARGET);
+            _xmlWriter.WriteStartElement(Elements.Target);
             WriteNameAttribute(args.Name);
             _xmlWriter.Flush();
         }
@@ -159,7 +127,7 @@ namespace SourceForge.NAnt {
         }
 
         public void TaskStarted(object obj, BuildEventArgs args) {
-            _xmlWriter.WriteStartElement(Elements.TASK);
+            _xmlWriter.WriteStartElement(Elements.Task);
             WriteNameAttribute(args.Name);
             _xmlWriter.Flush();
         }
@@ -169,15 +137,71 @@ namespace SourceForge.NAnt {
             _xmlWriter.Flush();
         }
 
+        #endregion Implementation of IBuildEventConsumer
+
+        #region Public Instance Methods
+
+        public string StripFormatting(string message) {
+            //looking for zero or more white space from front of line followed by
+            //one or more of just about anything between [ and ] followed by a message
+            //which we will capture. '    [blah] 
+            Regex r = new Regex(@"(?ms)^\s*?\[[\s\w\d]+\](.+)");
+
+            Match m = r.Match(message);
+            if(m.Success) {
+                return m.Groups[1].Captures[0].Value.Trim();
+            }
+            return message;
+        }
+
+        public bool IsJustWhiteSpace(string message) {
+            Regex r = new Regex(@"^\s*$");
+            return r.Match(message).Success;
+        }
+
+        #endregion Public Instance Methods
+
+        #region Private Instance Methods
+
+        private bool IsValidXml(string message) {
+            if (Regex.Match(message, @"^<.*>").Success) {
+                // validate xml
+                XmlValidatingReader reader = new XmlValidatingReader(message, XmlNodeType.Element, null);
+                try { 
+                    while (reader.Read()) {
+                    } 
+                } catch (Exception) { 
+                    return false; 
+                } finally { 
+                    reader.Close(); 
+                }
+                return true;
+            }
+            return false;
+        }
+        
+        private string StripCData(string message) {
+            string strippedMessage = Regex.Replace(message, @"<!\[CDATA\[", string.Empty);
+            return Regex.Replace(strippedMessage, @"\]\]>", string.Empty);
+        }
+
         private void WriteNameAttribute(string name) {
             _xmlWriter.WriteAttributeString("name", name);
         }
 
-        private void WriteStatus(string status) {
-            _xmlWriter.WriteStartElement(Elements.STATUS);
-            _xmlWriter.WriteAttributeString("value", status);
-            _xmlWriter.WriteEndElement();
+        #endregion Private Instance Methods
+
+        private class Elements {
+            public const string BuildResults = "buildresults";
+            public const string Message = "message";
+            public const string Target = "target";
+            public const string Task = "task";
+            public const string Status = "status";
         }
-        #endregion
+
+        private class Attributes {
+            public const string Project = "project";
+            public const string MessageType = "type";
+        }
     }
 }
