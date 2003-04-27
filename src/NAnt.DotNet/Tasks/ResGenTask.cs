f67198cb9168859f873eccab62896bee4f3c53d7
drieseng <gert.driesen@telenet.be>
2003-04-27 09:15:57 +0000

    - xml docs - code restructuring

--- src/NAnt.DotNet/Tasks/ResGenTask.cs
+++ src/NAnt.DotNet/Tasks/ResGenTask.cs
@@ -29,7 +29,7 @@ using SourceForge.NAnt.Attributes;
 
 namespace SourceForge.NAnt.Tasks {
     /// <summary>
-    /// Converts files from one resource format to another (wraps Microsoft's resgen.exe).
+    /// Converts files from one resource format to another.
     /// <note>
     ///     <para>
     ///         If no todir is specified, the resource file will be created next to the input file.
@@ -46,13 +46,18 @@ namespace SourceForge.NAnt.Tasks {
     /// </example>
     [TaskName("resgen")]
     public class ResGenTask : MsftFXSDKExternalProgramBase {
+        #region Private Instance Fields
 
-        string _arguments;
+        string _arguments = null;
         string _input = null; 
         string _output = null;        
         FileSet _resources = new FileSet();
         string _targetExt = "resources";
         string _toDir = null;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
 
         /// <summary>Input file to process.</summary>
         [TaskAttribute("input", Required=false)]
@@ -62,7 +67,7 @@ namespace SourceForge.NAnt.Tasks {
         [TaskAttribute("output", Required=false)]
         public string Output { get { return (_output == null) ? String.Empty : _output; } set {_output = value;} }
 
-        /// <summary>The target type ( usually resources).</summary>
+        /// <summary>The target type (usually resources).</summary>
         [TaskAttribute("target", Required=false)]
         public string TargetExt { get { return _targetExt; } set {_targetExt = value;} }
 
@@ -74,13 +79,34 @@ namespace SourceForge.NAnt.Tasks {
         [FileSet("resources")]
         public FileSet Resources { get { return _resources; } set { _resources = value; } }
                            
+        #endregion Public Instance Properties
 
-        public override string ProgramArguments { get { return _arguments; } }
-                
-        protected virtual void WriteOptions(TextWriter writer) {
+        #region Override implementation of ExternalProgramBase
+
+        /// <summary>
+        /// Gets the command line arguments for the external program.
+        /// </summary>
+        /// <value>
+        /// The command line arguments for the external program.
+        /// </value>
+        public override string ProgramArguments { 
+            get { return _arguments; } 
         }
-        
-        public override string ExeName {           
+
+        /// <summary>
+        /// Gets the name of the executable that should be used to launch the
+        /// external program.
+        /// </summary>
+        /// <value>
+        /// The name of the executable that should be used to launch the
+        /// external program.
+        /// </value>
+        /// <remarks>
+        /// If a current framework is defined, the name of the executable will
+        /// be retrieved from the configuration of the framework; otherwise the
+        /// <see cref="Task.Name" /> will be used.
+        /// </remarks>
+        protected override string ExeName {           
             get {
                 if (Project.CurrentFramework != null) {
                     return Project.CurrentFramework.ResGenToolName;
@@ -89,10 +115,102 @@ namespace SourceForge.NAnt.Tasks {
                 }
             }
         }
-                
+
+        /// <summary>
+        /// Gets a value indicating whether the external program should be executed
+        /// using a runtime engine, if configured.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the program should be executed using a runtime engine;
+        /// otherwise, <c>false</c>.
+        /// </value>
+        protected override bool UsesRuntimeEngine { 
+            get {                 
+                // TO-DO : uncomment this when monoresgen no longer crashes when run with the mono runtime.
+                //if (Project.CurrentFramework != null) {
+                //      if (Project.CurrentFramework.Name.IndexOf("mono", 0) != -1 ) { // remove hardcoded ness
+                //          return true;
+                //      }
+                //}                
+                return false;                
+            }
+        }
+
+        /// <summary>
+        /// Converts a single file or group of files.
+        /// </summary>
+        protected override void ExecuteTask() {
+            _arguments = "";
+            if (Resources.FileNames.Count > 0) {
+                if (Output != null && Output.Length != 0) {
+                    throw new BuildException("Output attribute is incompatible with fileset use.", Location);
+                }
+                foreach (string filename in Resources.FileNames) {
+                    string outputFile = getOutputFile(filename);
+
+                    if (NeedsCompiling(filename, outputFile)) {
+                        if (_arguments.Length == 0) {
+                            AppendArgument ("/compile");
+                        }
+                        AppendArgument(String.Format(CultureInfo.InvariantCulture, " \"{0},{1}\"", filename, outputFile));
+                    }
+                }
+            } else {
+                // Single file situation
+                if (Input == null) {
+                    throw new BuildException("Resource generator needs either an input attribute, or a non-empty fileset.", Location);
+                }
+
+                string inputFile = Path.GetFullPath(Path.Combine (BaseDirectory, Input));
+                string outputFile = getOutputFile(inputFile);
+
+                if (NeedsCompiling(inputFile, outputFile)) {
+                    AppendArgument(String.Format(CultureInfo.InvariantCulture, "\"{0}\" \"{1}\"", inputFile, outputFile));
+                }
+            }
+
+            if ( _arguments.Length > 0) {
+                // call base class to do the work
+                base.ExecuteTask();
+            }
+        }
+
+        #endregion Override implementation of ExternalProgramBase
+
+        #region Public Instance Methods
+
+        /// <summary>
+        /// Cleans up generated files.
+        /// </summary>
+        public void RemoveOutputs () {
+            foreach (string filename in Resources.FileNames) {
+                string outputFile = Path.ChangeExtension(filename, TargetExt);
+                if (filename != outputFile) {
+                    File.Delete (outputFile);
+                }
+                if (Input != null) {
+                    outputFile = Path.ChangeExtension(Input, TargetExt);
+                    if (Input != outputFile) {
+                        File.Delete (outputFile);
+                    }
+                }
+            }                     
+        }
+
+        #endregion Public Instance Methods
+
+        #region Protected Instance Methods
+
+        /// <summary>
+        /// Determines whether the specified input file needs to be compiled.
+        /// </summary>
+        /// <param name="input">The input file.</param>
+        /// <param name="output">The output file.</param>
+        /// <returns>
+        /// <c>true</c> if the input file need to be compiled; otherwise 
+        /// <c>false</c>.
+        /// </returns>
         protected virtual bool NeedsCompiling(string input, string output) {
-              // return true as soon as we know we need to compile
-  
               FileInfo outputFileInfo = new FileInfo(output);
               if (!outputFileInfo.Exists) {
                   return true;
@@ -107,51 +225,27 @@ namespace SourceForge.NAnt.Tasks {
                   return true;
               }
   
-              // if we made it here then we don't have to recompile
               return false;
         }
 
+        /// <summary>
+        /// Adds a command line argument to the command line for the external
+        /// program that is used to convert the resource files.
+        /// </summary>
+        /// <param name="s">The argument that should be added to the command line.</param>
         protected void AppendArgument(string s) {
             _arguments += s;
         }
         
-        // Process a single file or file group
-        protected override void ExecuteTask() {
-            _arguments = "";
-            if (Resources.FileNames.Count > 0) {
-                if (Output != null && Output.Length != 0) {
-                    throw new BuildException("Output attribute is incompatible with fileset use.", Location);
-                }
-                foreach ( string filename in Resources.FileNames ) {
-                    string outputFile = getOutputFile(filename);
+        #endregion Protected Instance Methods
 
-                    if (NeedsCompiling (filename, outputFile)) {
-                        if (_arguments.Length == 0) {
-                            AppendArgument ("/compile");
-                        }
-                        AppendArgument (String.Format(CultureInfo.InvariantCulture, " \"{0},{1}\"", filename, outputFile));
-                    }
-                }
-            } else {
-                // Single file situation
-                if (Input == null)
-                    throw new BuildException("Resource generator needs either an input attribute, or a non-empty fileset.", Location);
-
-                string inputFile = Path.GetFullPath(Path.Combine (BaseDirectory, Input));
-                string outputFile = getOutputFile(inputFile);
-
-                if (NeedsCompiling (inputFile, outputFile)) {
-                    AppendArgument (String.Format(CultureInfo.InvariantCulture, "\"{0}\" \"{1}\"", inputFile, outputFile));
-                }
-            }
-
-            if ( _arguments.Length > 0) {
-                // call base class to do the work
-                base.ExecuteTask();
-            }
-        }
-        
-        // Determine the full path and extension for the output file
+        #region Private Instance Methods
+       
+        /// <summary>
+        /// Determines the full path and extension for the output file.
+        /// </summary>
+        /// <param name="filename">The output file for which the full path and extension should be determined.</param>
+        /// <returns>The full path (with extensions) for the specified file.</returns>
         private string getOutputFile(string filename) {
             FileInfo fileInfo = new FileInfo(filename);
             string outputFile = "";
@@ -160,46 +254,16 @@ namespace SourceForge.NAnt.Tasks {
             if (Output == null ||  Output.Length == 0) {
                 if (ToDirectory == null || ToDirectory.Length == 0) {
                     outputFile = filename;
-                } 
-                else {
-                    outputFile = Path.Combine (ToDirectory, fileInfo.Name);
+                } else {
+                    outputFile = Path.Combine(ToDirectory, fileInfo.Name);
                 }
                 outputFile = Path.ChangeExtension( outputFile, TargetExt );
-            } 
-            else {
+            } else {
                 outputFile = Path.Combine (ToDirectory, Output);
             }
             return outputFile;
         }
         
-        /// <summary>
-        /// Clean up generated files
-        /// </summary>
-        public void RemoveOutputs () {
-            foreach ( string filename in Resources.FileNames ) {
-                string outputFile = Path.ChangeExtension( filename, TargetExt );
-                if ( filename != outputFile) {
-                    File.Delete (outputFile);
-                }
-                if (Input != null) {
-                    outputFile = Path.ChangeExtension( Input, TargetExt );
-                    if ( Input != outputFile) {
-                        File.Delete (outputFile);
-                    }
-                }
-            }                     
-        }
-
-        protected override bool UsesRuntimeEngine { 
-            get {                 
-                // TO-DO : uncomment this when monoresgen no longer crashes when run with the mono runtime.
-                //if (Project.CurrentFramework != null) {
-                //      if (Project.CurrentFramework.Name.IndexOf("mono", 0) != -1 ) { // remove hardcoded ness
-                //          return true;
-                //      }
-                //}                
-                return false;                
-            }
-        }
+        #endregion Private Instance Methods
     }
 }
