46daf1b9978c96fa8e802afa0357ee07ae2eaada
Scott Hernandez <skot@users.sourceforge.net>
2003-05-08 21:49:23 +0000

    Fails if no match is found. Clean up docs and added new example.

--- src/NAnt.Core/Tasks/RegexTask.cs
+++ src/NAnt.Core/Tasks/RegexTask.cs
@@ -19,6 +19,7 @@
 
 using System;
 using System.Collections;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 using SourceForge.NAnt.Attributes;
@@ -27,6 +28,7 @@ namespace SourceForge.NAnt.Tasks {
     /// <summary>
     /// Sets project properties based on the evaluatuion of a regular expression.
     /// </summary>
+    /// 
     /// <remarks>
     /// The &quot;pattern&quot; attribute must contain one or more 
     /// <a href="http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpcongroupingconstructs.asp">named grouping constructs</a>, 
@@ -35,20 +37,26 @@ namespace SourceForge.NAnt.Tasks {
     /// <note>In the build file, use the XML element <![CDATA[&lt;]]> to specify &lt;, and <![CDATA[&gt;]]> to specify &gt;.</note>
     /// <note>The named grouping construct must not contain any punctuation and it cannot begin with a number.</note>
     /// </remarks>
+    /// 
     /// <example>
-    /// <para>Find the last word in the given string and stores it property lastword.</para>
-    /// <code>
+    ///     <para>Find the last word in the given string and stores it property lastword.</para>
+    ///     <code>
     /// <![CDATA[
     /// <regex pattern="(?'lastword'\w+)$" input="This is a test sentence"/>
     /// <echo message="${lastword}"/>
     ///  ]]>
-    /// </code>
-    /// <para>Splits the full filename and extension of a filename.</para>
-    /// <code>
+    ///     </code>
+    ///     <para>Splits the full filename and extension of a filename.</para>
+    ///     <code>
     /// <![CDATA[
     /// <regex pattern="^(?'filename'.*)\.(?'extension'\w+)$" input="d:\Temp\SomeDir\SomeDir\bla.xml"/>
     ///  ]]>
-    /// </code>
+    ///     <para>Splits the path and the filename. (This checks for "/" or "\" as the path sep.)</para>
+    /// <![CDATA[
+    /// <regex pattern="^(?'path'.*(\\|/)|(/|\\))(?'file'.*)$" input="d:\Temp\SomeDir\SomeDir\bla.xml"/>
+    ///  ]]>
+    ///     </code>
+    ///     <para>Results in path=d:\Temp\SomeDir\SomeDir\ , file=bla.xml </para>
     /// </example>
     [TaskName("regex")]
     public class RegexTask : Task {
@@ -82,13 +90,18 @@ namespace SourceForge.NAnt.Tasks {
             Regex regex = new Regex(_pattern);
             Match match = regex.Match(_input);
 
+            if(match.Groups.Count == 0) {
+                string msg = string.Format(CultureInfo.InvariantCulture, "No match found for expr '{0}' in '{1}'", _pattern, _input);
+                throw new BuildException(msg, Location);
+            }
+
             // we start the iteration at 1, since the collection of groups 
             // always starts with a group which matches the entire input and is named '0'
             // this group is of no interest to us
             for (int i=1; i < match.Groups.Count; i++) {
                 string groupName = regex.GroupNameFromNumber(i);
 
-                Log.WriteLineIf(Verbose, "Setting property {0} to {1}", groupName, match.Groups[groupName].Value);
+                Log.WriteLineIf(Verbose, "{2}Setting property {0} to {1}", groupName, match.Groups[groupName].Value, LogPrefix);
                 Properties [ groupName ] = match.Groups[groupName].Value;
             }
         }
