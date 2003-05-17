d5c0c167639e03d998e373746c1a2f79b00d1e0c
drieseng <gert.driesen@telenet.be>
2003-05-17 14:09:03 +0000

    flush buffered events when build is finished

--- src/NAnt.Core/XmlLogger.cs
+++ src/NAnt.Core/XmlLogger.cs
@@ -83,6 +83,7 @@ namespace SourceForge.NAnt {
         /// </remarks>
         public void BuildFinished(object sender, BuildEventArgs e) {
             _xmlWriter.WriteEndElement();
+            _xmlWriter.Flush();
         }
 
         /// <summary>
