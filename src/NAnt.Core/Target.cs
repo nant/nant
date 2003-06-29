619b35d074650f10b47e7a2948f275d3cc85fa4c
Ian MacLean <ianm74@users.sourceforge.net>
2003-06-29 02:22:45 +0000

    added ability to define datatype references at target level. ( 2nd try)

--- src/NAnt.Core/Target.cs
+++ src/NAnt.Core/Target.cs
@@ -10,7 +10,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -38,7 +38,7 @@ namespace NAnt.Core {
 
         /// <summary> Public Constructor </summary>
         public Target() {
-             }
+        }
 
         private Target(Target t) : base((Element)t) {
             this._name = t._name;
