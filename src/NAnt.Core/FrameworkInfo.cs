2a23d593b2d172ed56486636ed73706ea6209751
drieseng <gert.driesen@telenet.be>
2003-07-03 19:07:23 +0000

    removed compiler-related properties, added additional checks, allow property expansion on framework-related properties; add framework-specific properties

--- src/NAnt.Core/FrameworkInfo.cs
+++ src/NAnt.Core/FrameworkInfo.cs
@@ -29,70 +29,102 @@ namespace NAnt.Core {
     public class FrameworkInfo {
         #region Private Instance Fields
 
-        string          _name;
-        string          _description;
-        string          _version;
-        string          _csharpCompilerName; // move this to task specific section..
-        string          _basicCompilerName;
-        string          _jsharpCompilerName;
-        string          _jscriptCompilerName;
-        string          _resgenToolName;         
-        DirectoryInfo   _frameworkDirectory;
-        DirectoryInfo   _sdkDirectory;
-        DirectoryInfo   _frameworkAssemblyDirectory;
-        FileInfo        _runtimEngine;
+        string              _name;
+        string              _description;
+        string              _version;
+        DirectoryInfo       _frameworkDirectory;
+        DirectoryInfo       _sdkDirectory;
+        DirectoryInfo       _frameworkAssemblyDirectory;
+        FileInfo            _runtimEngine;
+        PropertyDictionary  _properties = new PropertyDictionary();
 
         #endregion Private Instance Fields
 
         #region Public Instance Constructors
 
-        public FrameworkInfo(string name, 
-                             string description, 
-                             string version, 
-                             string frameworkDir, 
-                             string sdkDir, 
-                             string frameworkAssemblyDir,
-                             string csharpCompilerName,
-                             string basicCompilerName,
-                             string jsharpCompilerName,
-                             string jscriptCompilerName,
-                             string resgenToolName,
-                             string runtimeEngine) {
-            _name = name;
-            _description = description;
-            _version = version;           
-                        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrameworkInfo" /> class
+        /// with a name, description, version, runtime engine, directory information
+        /// and properties.
+        /// </summary>
+        /// <param name="name">The name of the framework.</param>
+        /// <param name="description">The description of the framework.</param>
+        /// <param name="version">The version number of the framework.</param>
+        /// <param name="frameworkDir">The directory of the framework.</param>
+        /// <param name="sdkDir">The directory containing the SDK tools for the framework, if available.</param>
+        /// <param name="frameworkAssemblyDir">The directory containing the system assemblies for the framework.</param>
+        /// <param name="runtimeEngine">The name of the runtime engine, if required.</param>
+        /// <param name="properties">Collection of framework specific properties.</param>
+        public FrameworkInfo(string name, string description, string version, 
+            string frameworkDir, string sdkDir, string frameworkAssemblyDir, 
+            string runtimeEngine, PropertyDictionary properties) {
+
+            if (name == null) {
+                throw new ArgumentNullException("name", "Framework name not configured.");
+            }
+
+            if (version == null) {
+                throw new ArgumentNullException("version", string.Format(
+                    CultureInfo.InvariantCulture, "Version not configured for framework {0}.", name));
+            }
+
+            if (frameworkDir == null) {
+                throw new ArgumentNullException("frameworkDir", string.Format(
+                    CultureInfo.InvariantCulture, "Framework directory not configured for framework {0}.", name));
+            }
+
+            if (frameworkAssemblyDir == null) {
+                throw new ArgumentNullException("frameworkAssemblyDir", string.Format(
+                    CultureInfo.InvariantCulture, "Framework assembly directory not configured for framework {0}.", name));
+            }
+
+            if (properties == null) {
+                throw new ArgumentNullException("properties", string.Format(
+                    CultureInfo.InvariantCulture, "Framework properties not configured for framework {0}.", name));
+            }
+
+            _name = properties.ExpandProperties(name, Location.UnknownLocation);
+            _description = properties.ExpandProperties(description, Location.UnknownLocation);
+            _version = properties.ExpandProperties(version, Location.UnknownLocation);
+            _properties = properties;
+
+            frameworkDir = properties.ExpandProperties(frameworkDir, Location.UnknownLocation);
             if (Directory.Exists(frameworkDir)) {
                 _frameworkDirectory = new DirectoryInfo(frameworkDir);
             } else {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "frameworkDir {0} does not exist", frameworkDir) );
+                throw new ArgumentException(string.Format(
+                    CultureInfo.InvariantCulture, "Framework directory {0} does not exist.", frameworkDir));
             }
-            
+
+            frameworkAssemblyDir = properties.ExpandProperties(frameworkAssemblyDir, (Location) null);
             if (Directory.Exists(frameworkAssemblyDir)) {
                 _frameworkAssemblyDirectory = new DirectoryInfo(frameworkAssemblyDir);
             } else {
-                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "framework Assembly Dir {0} does not exist", frameworkAssemblyDir) );
+                throw new ArgumentException(string.Format(
+                    CultureInfo.InvariantCulture, "Framework assembly directory {0} does not exist.", frameworkAssemblyDir));
             }
-                                   
-            if (sdkDir != null && Directory.Exists(sdkDir)) {
-                _sdkDirectory = new DirectoryInfo(sdkDir);
-            }           
+
+            try {
+                sdkDir = properties.ExpandProperties(sdkDir, Location.UnknownLocation);
+                if (sdkDir != null && Directory.Exists(sdkDir)) {
+                    _sdkDirectory = new DirectoryInfo(sdkDir);
+                }
+            } catch (BuildException) {
+                // do nothing with this exception as a framework is still
+                // considered valid if the sdk directory is not available
+                // or not configured correctly
+            }
 
             // if runtime engine is blank assume we aren't using one
             if (runtimeEngine != null && runtimeEngine.Length != 0) {
                 string runtimeEnginePath = _frameworkDirectory.FullName + Path.DirectorySeparatorChar + runtimeEngine;
-                if ( File.Exists(runtimeEnginePath ) ){
-                    _runtimEngine = new FileInfo( runtimeEnginePath );
+                if (File.Exists(runtimeEnginePath)) {
+                    _runtimEngine = new FileInfo(runtimeEnginePath);
                 } else {
-                    throw new ArgumentException(string.Format( CultureInfo.InvariantCulture, "runtime Engine {0} does not exist", runtimeEnginePath )  );            
+                    throw new ArgumentException(string.Format(
+                        CultureInfo.InvariantCulture, "Runtime engine {0} does not exist.", runtimeEnginePath));
                 }
             }
-            // Validate that these tools exist ..
-            _csharpCompilerName = csharpCompilerName;
-            _basicCompilerName = basicCompilerName;
-            _jsharpCompilerName = jsharpCompilerName;
-            _jscriptCompilerName = jscriptCompilerName;
-            _resgenToolName = resgenToolName;
         }
 
         #endregion Public Instance Constructors
@@ -100,86 +132,88 @@ namespace NAnt.Core {
         #region Public Instance Properties
 
         /// <summary>
-        /// Gets the name of this framework.
+        /// Gets the name of the framework.
         /// </summary>
+        /// <value>The name of the framework.</value>
         public string Name {
             get { return _name; }
         }
 
         /// <summary>
-        /// Gets the description of this framework.
+        /// Gets the description of the framework.
         /// </summary>
+        /// <value>
+        /// The description of the framework.
+        /// </value>
         public string Description {
             get { return _description; }
         }
         
         /// <summary>
-        /// Gets the version of this framework.
+        /// Gets the version of the framework.
         /// </summary>
+        /// <value>
+        /// The version of the framework.
+        /// </value>
         public string Version {
             get { return _version; }
         }
         
         /// <summary>
-        /// Gets the name of the C# compiler for this framework.
+        /// Gets the base directory of the framework tools for the framework.
         /// </summary>
-        public string CSharpCompilerName {
-            get { return _csharpCompilerName; }
-        }
-
-        /// <summary>
-        /// Gets the name of the Basic compiler for this framework.
-        /// </summary>
-        public string BasicCompilerName {
-            get { return _basicCompilerName; }
-        }
-
-        /// <summary>
-        /// Gets the name of the J# compiler for this framework.
-        /// </summary>
-        public string JSharpCompilerName {
-            get { return _jsharpCompilerName; }
-        }
-
-        /// <summary>
-        /// Gets the name of the JScript compiler for this framework.
-        /// </summary>
-        public string JScriptCompilerName {
-            get { return _jscriptCompilerName; }
-        }
-        
-        /// <summary>
-        /// Gets the name of the resgen tool for this framework.
-        /// </summary>
-        public string ResGenToolName {
-            get { return _resgenToolName; }
-        }
-                
-        /// <summary>
-        /// Gets the base directory of the framework tools for this framework.
-        /// </summary>
+        /// <value>
+        /// The base directory of the framework tools for the framework.
+        /// </value>
         public DirectoryInfo FrameworkDirectory {
             get { return _frameworkDirectory; }
         }
         /// <summary>
-        /// Gets the path to the runtime engine for this framework. (not required for many frameworks )
+        /// Gets the path to the runtime engine for this framework.
         /// </summary>
+        /// <value>
+        /// The path to the runtime engine for the framework or null if no
+        /// runtime gine is configured for the framework.
+        /// </value>
         public FileInfo RuntimeEngine {
             get { return _runtimEngine; }
         }
        
         /// <summary>
-        /// Gets the directory where the system assemblies are located.
+        /// Gets the directory where the system assemblies for the framework 
+        /// are located.
         /// </summary>
+        /// <value>
+        /// The directory where the system assemblies for the framework are 
+        /// located.
+        /// </value>
         public DirectoryInfo FrameworkAssemblyDirectory {
             get { return _frameworkAssemblyDirectory; }
         }
         
         /// <summary>
-        /// Gets the directory containing the framework SDK tools.
+        /// Gets the directory containing the SDK tools for the framework.
         /// </summary>
+        /// <value>
+        /// The directory containing the SDK tools for the framework or a null 
+        /// refererence if the sdk directory
+        /// </value>
         public DirectoryInfo SdkDirectory {
             get { return _sdkDirectory; }
+        }
+
+        /// <summary>
+        /// Gets the properties defined for this framework.
+        /// </summary>
+        /// <value>The properties defined for this framework.</value>
+        /// <remarks>
+        /// <para>
+        /// This is the collection of properties for this framework in the 
+        /// NAnt configuration file.
+        /// </para>
+        /// </remarks>
+        public PropertyDictionary Properties {
+            get { return _properties; }
         }
 
         #endregion Public Instance Properties
