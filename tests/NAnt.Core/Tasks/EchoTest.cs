80f5d1b761b38c7ae3a95d56e66ac598073f7e8c
drieseng <gert.driesen@telenet.be>
2003-07-20 09:02:06 +0000

    added tests for new Level attribute

--- tests/NAnt.Core/Tasks/EchoTest.cs
+++ tests/NAnt.Core/Tasks/EchoTest.cs
@@ -14,47 +14,85 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+//
 // Scott Hernandez (ScottHernandez@hotmail.com)
+// Gert Driesen (gert.driesen@ardatis.com)
 
 using System;
-using System.Xml;
 
 using NUnit.Framework;
 
+using NAnt.Core;
+
 namespace Tests.NAnt.Core.Tasks {
     /// <summary>
-    /// Tests the Echo test.
+    /// Tests the Echo task.
     /// </summary>
     [TestFixture]
     public class EchoTest : BuildTestBase {
-    
-    	[SetUp]
-    	protected override void SetUp() {
-    		base.SetUp();
-    	}
+        [SetUp]
+        protected override void SetUp() {
+            base.SetUp();
+        }
         
         [Test]
-        public void Test_Echo() {
-            //TODO: Use this once we have fixed the LocationMap issue
-            /*
-            XmlDocument doc = new XmlDocument();
-            XmlElement project = doc.CreateElement("project");
-            XmlElement echo = doc.CreateElement("echo");
-            echo.SetAttribute("message","Go Away!");
-            project.AppendChild(echo);
-            doc.AppendChild(project);
-
-            Project p = new Project(doc);
-            
-            string result = ExecuteProject(p);
-            */
+        public void Test_EchoDefaultProjectInfo() {
             string _xml = @"
                     <project>
                         <echo message='Go Away!'/>
                     </project>";
-            string result = RunBuild(_xml);            
+            string result = RunBuild(_xml);
             Assertion.Assert("Echo message missing:" + result, result.IndexOf("Go Away!") != -1);
+        }
+
+        [Test]
+        public void Test_EchoDebugProjectInfo() {
+            string _xml = @"
+                    <project>
+                        <echo message='Go Away!' level='Debug' />
+                    </project>";
+            string result = RunBuild(_xml, Level.Info);
+            Assertion.Assert("Debug echo should not be output when Project level is Info.", result.IndexOf("Go Away!") == -1);
+        }
+
+        [Test]
+        public void Test_EchoWarningProjectInfo() {
+            string _xml = @"
+                    <project>
+                        <echo level='Warning'>Go Away!</echo>
+                    </project>";
+            string result = RunBuild(_xml, Level.Info);
+            Assertion.Assert("Warning echo should be output when Project level is Info.", result.IndexOf("Go Away!") != -1);
+        }
+
+        [Test]
+        public void Test_EchoWarningProjectError() {
+            string _xml = @"
+                    <project>
+                        <echo level='Warning'>Go Away!</echo>
+                    </project>";
+            string result = RunBuild(_xml, Level.Error);
+            Assertion.Assert("Warning echo should not be output when Project level is Error.", result.IndexOf("Go Away!") == -1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(TestBuildException))]
+        public void Test_EchoInvalidLevel() {
+            string _xml = @"
+                    <project>
+                        <echo message='Go Away!' level='Invalid' />
+                    </project>";
+            string result = RunBuild(_xml, Level.Error);
+        }
+
+        [Test]
+        [ExpectedException(typeof(TestBuildException))]
+        public void Test_EchoMessageAndInlineContent() {
+            string _xml = @"
+                    <project>
+                        <echo message='Go Away!' level='Debug'>Go Away!</echo>
+                    </project>";
+            string result = RunBuild(_xml, Level.Info);
         }
     }
 }
