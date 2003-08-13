66dc15518aed1b6ad90f22eafdf67e7abc48075c
drieseng <gert.driesen@telenet.be>
2003-08-13 13:55:34 +0000

    Arjen Poutsma's wrapper for regsvcs.

---
+++ src/NAnt.DotNet/Tasks/RegSvcsTask.cs
@@ -0,0 +1,321 @@
+// NAnt - A .NET build tool
+// Copyright (C) 2001-2003 Gerry Shaw
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// Arjen Poutsma (poutsma@yahoo.com)
+
+using System;
+using System.Globalization;
+using System.Text;
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Tasks;
+using NAnt.Core.Types;
+
+namespace NAnt.DotNet.Tasks {
+    /// <summary>
+    /// Installs or removes .NET Services.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This tasks provides the same functionality as the <c>regsvcs</c> tool 
+    /// provided in the .NET SDK.
+    /// </para>
+    /// <para>
+    /// It performs the following actions: 
+    /// <list type="bullet">
+    ///   <item>
+    ///     <description>Loads and registers an assembly.</description>
+    ///   </item>
+    ///   <item>
+    ///     <description>Generates, registers, and installs a type library into a specified COM+ application.</description>
+    ///   </item>
+    ///   <item>
+    ///     <description>Configures services that are added programmatically to your class.</description>
+    ///   </item>
+    /// </list>
+    /// Refer to the <a href="ms-help://MS.NETFrameworkSDK/cptools/html/cpgrfnetservicesinstallationutilityregsvcsexe.htm">.NET Services Installation Tool (Regsvcs.exe)</a> for more information.
+    /// </para>
+    /// </remarks>
+    /// <example>
+    /// <para>
+    /// Adds all public classes contained in <c>myTest.dll</c> to a COM+ application 
+    /// and produces the <c>myTest.tlb</c> type library. If the application already 
+    /// exists, it is overwritten.
+    /// </para>
+    /// <code>
+    ///     <![CDATA[
+    /// <regsvcs action="FindOrCreate" assembly="myTest.dll" />
+    ///     ]]>
+    /// </code>
+    /// <para>
+    /// Adds all public classes contained in <c>myTest.dll</c> to <c>myTargetApp</c> 
+    /// and produces the <c>myTest.tlb</c> type library. If the application already 
+    /// exists, it is overwritten.
+    /// </para>
+    /// <code>
+    ///     <![CDATA[
+    /// <regsvcs action="FindOrCreate" assembly="myTest.dll" application="myTargetApp" />
+    ///     ]]>
+    /// </code>
+    /// <para>
+    /// Adds all public classes contained in <c>myTest.dll</c> to a COM+ application 
+    /// and produces the <c>myTest.tlb</c> type library. A new application is always 
+    /// created.
+    /// </para>
+    /// <code>
+    ///     <![CDATA[
+    /// <regsvcs action="Create" assembly="myTest.dll" />
+    ///     ]]>
+    /// </code>
+    /// <para>
+    /// Uninstalls the COM+ application contained in <c>myTest.dll</c></para>
+    /// <code>
+    ///     <![CDATA[
+    /// <regsvcs action="Uninstall" assembly="myTest.dll" />
+    ///     ]]>
+    /// </code>
+    /// </example>
+    [TaskName("regsvcs")]
+    [ProgramLocation(LocationType.FrameworkDir)]
+    public class RegsvcsTask : ExternalProgramBase {
+        /// <summary>
+        /// Defines the possible actions for a .NET Service.
+        /// </summary>
+        public enum ActionType {
+            /// <summary>
+            /// Finds or creates the target application.
+            /// </summary>
+            FindOrCreate,
+
+            /// <summary>
+            /// Creates the target application.
+            /// </summary>
+            Create,
+
+            /// <summary>
+            /// Uninstalls the target application.
+            /// </summary>
+            Uninstall
+        }
+
+        #region Private Instance Fields
+
+        private StringBuilder _argumentBuilder = null;
+        private ActionType _action = ActionType.FindOrCreate;
+        private string _assemblyName = null;
+        private string _applicationName = null;
+        private string _tlb = null;
+        private bool _existingTlb = false;
+        private bool _existingApplication = false;
+        private bool _noreconfig = false;
+        private bool _componentsOnly = false;
+        private string _partitionName = null;
+        private bool _quiet = true;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// Defines the action to take with the assembly. Supported actions are: 
+        /// <c>FindOrCreate</c> (the default), <c>Create</c>, or <c>Uninstall</c>.
+        /// </summary>
+        [TaskAttribute("action")]
+        public ActionType Action {
+            get { return _action; }
+            set { 
+                if (!Enum.IsDefined(typeof(ActionType), value)) {
+                    throw new ArgumentException(string.Format(
+                        CultureInfo.InvariantCulture, "An invalid action {0} was specified.", 
+                        value)); 
+                } else {
+                    this._action = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The source assembly file.
+        /// </summary>
+        /// <remarks>
+        /// The assembly must be signed with a strong name.
+        /// </remarks>
+        [TaskAttribute("assembly", Required=true)]
+        public string AssemblyName {
+            get { return (_assemblyName != null) ? Project.GetFullPath(_assemblyName) : null; }
+            set { _assemblyName = SetStringValue(value); }
+        }
+
+        /// <summary>
+        /// Specifies the type library file to install.
+        /// </summary>
+        [TaskAttribute("tlb")]
+        public string TypeLibrary {
+            get { return (_tlb != null) ? Project.GetFullPath(_tlb) : null; }
+            set { _tlb = SetStringValue(value); }
+        }
+
+        /// <summary>
+        /// Uses an existing type library.
+        /// </summary>
+        [TaskAttribute("existingtlb")]
+        [BooleanValidator]
+        public bool ExistingTypeLibrary {
+            get { return _existingTlb; }
+            set { _existingTlb = value; }
+        }
+
+        /// <summary>
+        /// Do not reconfigure an existing target application.
+        /// </summary>
+        [TaskAttribute("noreconfig")]
+        [BooleanValidator]
+        public bool NoReconfig {
+            get { return _noreconfig; }
+            set {_noreconfig = value; }
+        }
+
+        /// <summary>
+        /// Configures components only; ignores methods and interfaces.
+        /// </summary>
+        [TaskAttribute("componentsonly")]
+        [BooleanValidator]
+        public bool ComponentsOnly {
+            get { return _componentsOnly; }
+            set { _componentsOnly = value; }
+        }
+
+        /// <summary>
+        /// Expect an existing application.
+        /// </summary>
+        [TaskAttribute("existingapp")]
+        [BooleanValidator]
+        public bool ExistingApplication {
+            get { return _existingApplication; }
+            set { _existingApplication = value; }
+        }
+
+        /// <summary>
+        /// Specifies the name of the COM+ application to either find or create.
+        /// </summary>
+        [TaskAttribute("application")]
+        public string ApplicationName {
+            get { return _applicationName; }
+            set { _applicationName = SetStringValue(value); }
+        }
+
+        /// <summary>
+        /// Specifies the name or id of the COM+ application to either find or 
+        /// create.
+        /// </summary>
+        [TaskAttribute("partition")]
+        public string PartitionName {
+            get { return _partitionName; }
+            set { _partitionName = SetStringValue(value); }
+        }
+
+        /// <summary>
+        /// Specifies whether non-error output should be suppressed.  Default
+        /// is <c>true</c>.
+        /// </summary>
+        [TaskAttribute("quiet")]
+        public bool Quiet {
+            get { return _quiet; }
+            set { _quiet = value; }
+        }
+
+        #endregion Public Instance Properties
+
+        #region Override implementation of ExternalProgramBase
+
+        /// <summary>
+        /// Gets the command-line arguments for the external program.
+        /// </summary>
+        /// <value>
+        /// The command-line arguments for the external program.
+        /// </value>
+        public override string ProgramArguments {
+            get { return _argumentBuilder.ToString(); }
+        }
+
+        /// <summary>
+        /// Performs the specified action.
+        /// </summary>
+        protected override void ExecuteTask() {
+            _argumentBuilder = new StringBuilder();
+
+            switch (Action) {
+                case ActionType.Create:
+                    _argumentBuilder.Append("/c ");
+                    Log(Level.Info, "Creating COM+ application '{0}'.", AssemblyName);
+                    break;
+                case ActionType.FindOrCreate:
+                    _argumentBuilder.Append("/fc ");
+                    Log(Level.Info, "Finding or creating COM+ application '{0}'.", AssemblyName);
+                    break;
+                case ActionType.Uninstall:
+                    _argumentBuilder.Append("/u ");
+                    Log(Level.Info,"Uninstalling COM+ application '{0}'.", AssemblyName);
+                    break;
+            }
+
+            if (TypeLibrary != null) {
+                _argumentBuilder.AppendFormat("\"/tlb:{0}\" ", TypeLibrary);
+            }
+
+            if (ExistingTypeLibrary) {
+                _argumentBuilder.Append("/extlb ");
+            }
+
+            if (NoReconfig) {
+                _argumentBuilder.Append("/noreconfig ");
+            }
+
+            if (ComponentsOnly) {
+                _argumentBuilder.Append("/componly ");
+            }
+
+            if (ApplicationName != null) {
+                _argumentBuilder.AppendFormat("\"/appname:{0}\" ", ApplicationName);
+            }
+
+            if (ExistingApplication) {
+                _argumentBuilder.Append("/exapp ");
+            }
+
+            if (PartitionName != null) {
+                _argumentBuilder.AppendFormat("\"/parname:{0}\" ", PartitionName);
+            }
+
+            if (Quiet) {
+                _argumentBuilder.Append("/quiet ");
+            }
+
+            // suppresses the Microsoft startup banner display
+            _argumentBuilder.Append("/nologo ");
+
+            // output the assembly name enclosed with quotes
+            _argumentBuilder.Append("\"" + AssemblyName + "\"");
+
+            // call base class to do perform the actual call
+            base.ExecuteTask();
+        }
+
+        #endregion Override implementation of ExternalProgramBase
+    }
+}
