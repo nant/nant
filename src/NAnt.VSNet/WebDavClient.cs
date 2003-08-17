384bdcda4cf8527a873f449cf539f926d69aa0ec
drieseng <gert.driesen@telenet.be>
2003-08-17 08:40:22 +0000

    code restructure, removed use of hungarian naming scheme for private instance fields and method parameters

--- src/NAnt.VSNet/WebDavClient.cs
+++ src/NAnt.VSNet/WebDavClient.cs
@@ -1,5 +1,5 @@
 // NAnt - A .NET build tool
-// Copyright (C) 2001-2002 Gerry Shaw
+// Copyright (C) 2001-2003 Gerry Shaw
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,113 +16,128 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 using System;
-using System.Net;
 using System.IO;
+using System.Net;
 
-namespace NAnt.VSNet.Tasks {
-    /// <summary>
-    /// Summary description for WebDavClient.
-    /// </summary>
+namespace NAnt.VSNet {
     public class WebDavClient {
-        public WebDavClient( Uri uriBase ) {
-            _strWebProjectBaseUrl = uriBase.ToString();
+        #region Public Instance Constructors
+
+        public WebDavClient(Uri uriBase) {
+            _webProjectBaseUrl = uriBase.ToString();
         }
 
-        public void UploadFile( string strLocalFilename, string strRemoteFilename ) {
-            WebRequest request = WebRequest.Create( _strWebProjectBaseUrl + "/" + strRemoteFilename );
-            Console.WriteLine( request.RequestUri.ToString() );
+        #endregion Public Instance Constructors
+
+        #region Public Instance Methods
+
+        public void UploadFile(string localFileName, string remoteFileName) {
+            WebRequest request = WebRequest.Create(_webProjectBaseUrl + "/" + remoteFileName);
+            Console.WriteLine(request.RequestUri.ToString());
             request.Method = "PUT";
-            request.Headers.Add( "Translate: f" );
+            request.Headers.Add("Translate: f");
             request.Credentials = CredentialCache.DefaultCredentials;
 
-            FileInfo fi = new FileInfo( strLocalFilename );
+            FileInfo fi = new FileInfo(localFileName);
             request.ContentLength = fi.Length;
 
             int bufferSize = 100 * 1024;
             byte[] buffer = new byte[bufferSize];
-            using ( FileStream fsInput = new FileStream( fi.FullName, FileMode.Open ) ) {
-                using ( Stream s = request.GetRequestStream() ) {
+            using (FileStream fsInput = new FileStream(fi.FullName, FileMode.Open)) {
+                using (Stream s = request.GetRequestStream()) {
                     int nRead;
                     do {
-                        nRead = fsInput.Read( buffer, 0, bufferSize );
-                        s.Write( buffer, 0, nRead );
+                        nRead = fsInput.Read(buffer, 0, bufferSize);
+                        s.Write(buffer, 0, nRead);
                     }
-                    while ( nRead > 0 );
+                    while (nRead > 0);
                 }
             }
 
             buffer = null;
             try {
-                using ( request.GetResponse() ) { }
-            }
-            catch ( WebException we ) {
+                using (request.GetResponse()) {
+                }
+            } catch (WebException we) {
                 HttpWebResponse hwr = ( HttpWebResponse )we.Response;
                 
-                if ( ( int )hwr.StatusCode != 423 )
+                if ((int) hwr.StatusCode != 423) {
                     throw;
+                }
             }
         }
 
-        public void DeleteFile( string strLocalFilename, string strRemoteFilename ) {
-            WebRequest request = WebRequest.Create( _strWebProjectBaseUrl + "/" + strRemoteFilename );
-            Console.WriteLine( request.RequestUri.ToString() );
+        public void DeleteFile(string localFileName, string remoteFileName) {
+            WebRequest request = WebRequest.Create(_webProjectBaseUrl + "/" + remoteFileName);
+            Console.WriteLine(request.RequestUri.ToString());
             request.Method = "DELETE";
-            request.Headers.Add( "Translate: f" );
+            request.Headers.Add("Translate: f");
             request.Credentials = CredentialCache.DefaultCredentials;
 
-            using ( request.GetResponse() ) { }
+            using (request.GetResponse()) {
+            }
         }
 
-        public void DownloadFile( string strLocalFilename, string strRemoteFilename ) {
-            WebRequest request = WebRequest.Create( _strWebProjectBaseUrl + "/" + strRemoteFilename );
-            Console.WriteLine( request.RequestUri.ToString() );
+        public void DownloadFile(string localFileName, string remoteFileName) {
+            WebRequest request = WebRequest.Create(_webProjectBaseUrl + "/" + remoteFileName);
+            Console.WriteLine(request.RequestUri.ToString());
             request.Method = "GET";
-            request.Headers.Add( "Translate: f" );
+            request.Headers.Add("Translate: f");
             request.Credentials = CredentialCache.DefaultCredentials;
-            FileInfo fi = new FileInfo( strLocalFilename );
-            if ( !Directory.Exists( fi.DirectoryName ) )
-                Directory.CreateDirectory( fi.DirectoryName );
+            FileInfo fi = new FileInfo(localFileName);
+            if (!Directory.Exists(fi.DirectoryName)) {
+                Directory.CreateDirectory(fi.DirectoryName);
+            }
 
             int bufferSize = 100 * 1024;
             byte[] buffer = new byte[bufferSize];
-            using ( FileStream fsOutput = new FileStream( fi.FullName, FileMode.OpenOrCreate ) ) {
-                using ( Stream s = request.GetResponse().GetResponseStream() ) {
+            using (FileStream fsOutput = new FileStream(fi.FullName, FileMode.OpenOrCreate)) {
+                using (Stream s = request.GetResponse().GetResponseStream()) {
                     int nRead;
                     do {
-                        nRead = s.Read( buffer, 0, bufferSize );
-                        fsOutput.Write( buffer, 0, nRead );
-                    }
-                    while ( nRead > 0 );
+                        nRead = s.Read(buffer, 0, bufferSize);
+                        fsOutput.Write(buffer, 0, nRead);
+                    } while (nRead > 0);
                 }
             }
 
             buffer = null;
         }
 
-        public string GetFileContents( string strRemoteFilename ) {
-            WebRequest request = WebRequest.Create( _strWebProjectBaseUrl + "/" + strRemoteFilename );
-            Console.WriteLine( request.RequestUri.ToString() );
+        public string GetFileContents(string remoteFileName) {
+            WebRequest request = WebRequest.Create(_webProjectBaseUrl + "/" + remoteFileName);
+            Console.WriteLine(request.RequestUri.ToString());
             request.Method = "GET";
-            request.Headers.Add( "Translate: f" );
+            request.Headers.Add("Translate: f");
             request.Credentials = CredentialCache.DefaultCredentials;
 
-            using ( StreamReader sr = new StreamReader( request.GetResponse().GetResponseStream() ) ) {
+            using (StreamReader sr = new StreamReader(request.GetResponse().GetResponseStream())) {
                 return sr.ReadToEnd();
             }
         }
 
-        public static string GetFileContentsStatic( string strRemoteFilename ) {
-            WebRequest request = WebRequest.Create( strRemoteFilename );
-            Console.WriteLine( request.RequestUri.ToString() );
+        #endregion Public Instance Methods
+
+        #region Public Static Methods
+
+        public static string GetFileContentsStatic(string remoteFileName) {
+            WebRequest request = WebRequest.Create(remoteFileName);
+            Console.WriteLine(request.RequestUri.ToString());
             request.Method = "GET";
-            request.Headers.Add( "Translate: f" );
+            request.Headers.Add("Translate: f");
             request.Credentials = CredentialCache.DefaultCredentials;
 
-            using ( StreamReader sr = new StreamReader( request.GetResponse().GetResponseStream() ) ) {
+            using (StreamReader sr = new StreamReader(request.GetResponse().GetResponseStream())) {
                 return sr.ReadToEnd();
             }
         }
 
-        private string _strWebProjectBaseUrl;
+        #endregion Public Static Methods
+
+        #region Private Instance Fields
+
+        private string _webProjectBaseUrl;
+
+        #endregion Private Instance Fields
     }
 }
