197ce827f8d5784d30ae84895a3bacb2a8b4e44c
drieseng <gert.driesen@telenet.be>
2003-08-30 08:17:48 +0000

    add StringValidatorAttribute which allows task developers to specify whether an empty string value is allowed for a certain task attribute.
    Currently, when the Required property of the TaskAttribute(Attribute) is set to "true", no check is being performed whether the attribute on the build element contains a value or not, which means specifying an empty value for an required attribute on a task would allow it to pass the Required check.
    
    We have considered changing the behaviour of the Required attribute so that the test would also fail if an empty value was specified, but this could break the behaviour of existing (third party) tasks, so we decided against this.
    
    Instead, we've added a StringValidator(Attribute) that has an AllowEmpty property (which is true by default) that can be set to "false", in order to have the build fail when an empty value is specified for an attribute that requires a (non-empty) value.

---
+++ src/NAnt.Core/Attributes/StringValidatorAttribute.cs
@@ -0,0 +1,94 @@
+// NAnt - A .NET build tool
+// Copyright (C) 2003 Gerry Shaw
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Gert Driesen (gert.driesen@ardatis.com)
+
+using System;
+using System.Globalization;
+
+using NAnt.Core.Util;
+
+namespace NAnt.Core.Attributes {
+    /// <summary>
+    /// Used to indicate whether a <see cref="string" /> property should allow 
+    /// an empty string value or not.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited=true)]
+    public sealed class StringValidatorAttribute : ValidatorAttribute {
+        #region Public Instance Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringValidatorAttribute" /> 
+        /// class.
+        /// </summary>
+        public StringValidatorAttribute() {
+        }
+
+        #endregion Public Instance Constructors
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether an empty string or
+        /// <see langword="null" /> should be a considered a valid value.
+        /// </summary>
+        /// <value>
+        /// <see langword="true" /> if an empty string or <see langword="null" />
+        /// should be considered a valid value; otherwise, <see langword="false" />.
+        /// </value>
+        /// <remarks>
+        /// The default value is <see langword="true" />.
+        /// </remarks>
+        public bool AllowEmpty {
+            get { return _allowEmpty; }
+            set { _allowEmpty = value; }
+        }
+
+        #endregion Public Instance Properties
+
+        #region Override implementation of ValidatorAttribute
+
+        /// <summary>
+        /// Checks if the specified value adheres to the rules defined by the 
+        /// properties of the <see cref="StringValidatorAttribute" />.
+        /// </summary>
+        /// <param name="value">The value to be checked.</param>
+        /// <exception cref="ValidationException"><paramref name="value" /> is an empty string value and <see cref="AllowEmpty" /> is set to <see langword="false" />.</exception>
+        public override void Validate(object value) {
+            string valueString;
+
+            try {
+                valueString = Convert.ToString(value, CultureInfo.InvariantCulture);
+            } catch (Exception ex) {
+                throw new ValidationException(string.Format(CultureInfo.InvariantCulture, 
+                    "Cannot resolve to '{0}' to string value.", value.ToString()), ex);
+            }
+
+            if (!AllowEmpty && StringUtils.IsNullOrEmpty(valueString)) {
+                throw new ValidationException("An empty value is not allowed.");
+            }
+        }
+
+        #endregion Override implementation of ValidatorAttribute
+
+        #region Private Instance Fields
+
+        private bool _allowEmpty = true;
+
+        #endregion Private Instance Fields
+    }
+}
