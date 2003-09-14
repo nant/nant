69249ff71958a59053d8441284824ad3e2b078b5
drieseng <gert.driesen@telenet.be>
2003-09-14 13:34:44 +0000

    second try

--- src/NAnt.Compression/Tasks/UnZipTask.cs
+++ src/NAnt.Compression/Tasks/UnZipTask.cs
@@ -62,7 +62,7 @@ namespace NAnt.Zip.Tasks {
         }
 
         /// <summary>
-        /// The directory where the expanded files.should be stored.
+        /// The directory where the expanded files should be stored.
         /// </summary>
         [TaskAttribute("todir", Required=false)]
         public string ToDirectory {
