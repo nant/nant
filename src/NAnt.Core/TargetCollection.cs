d2cd9fdff951f964def02ba126f48da5bf78a62d
drieseng <gert.driesen@telenet.be>
2003-09-15 06:33:46 +0000

    - overridde ToString method to get comma-separated list of the targets in the collection - added ToString(string separator) method to allow other separator character to be specified

--- src/NAnt.Core/TargetCollection.cs
+++ src/NAnt.Core/TargetCollection.cs
@@ -26,7 +26,6 @@ namespace NAnt.Core {
 
     public class TargetCollection : ArrayList {
         private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         public virtual int Add(Target t){
             //throw an exception if an attempt is made to add a null target
             if(t == null) {
@@ -55,9 +54,44 @@ namespace NAnt.Core {
             return null;
         }
 
+        /// <summary>
+        /// Gets the names of the targets in the <see cref="TargetCollection" />
+        /// combined into one list separated by the given <see cref="string" />.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that contains a list of the names of the 
+        /// targets in the <see cref="TargetCollection" />, separated by
+        /// the specified <paramref name="separator" />.
+        /// </returns>
+        public string ToString(string separator) {
+            string[] targetNames = new string[Count];
+
+            for (int i = 0; i < Count; i++) {
+                targetNames[i] = ((Target) this[i]).Name;
+            }
+
+            return string.Join(separator, targetNames);
+        }
+
         public override int Add(object value) {
             //call typed version above.
             return Add(value as Target);
         }
+
+        #region Override implementation of Object
+
+        /// <summary>
+        /// Gets the names of the targets in the <see cref="TargetCollection" />
+        /// combined into one comma-separated list.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that contains a comma-separated list of the
+        /// names of the targets in the <see cref="TargetCollection" />.
+        /// </returns>
+        public override string ToString() {
+            return ToString(", ");
+        }
+
+        #endregion Override implementation of Object
     }
 }
