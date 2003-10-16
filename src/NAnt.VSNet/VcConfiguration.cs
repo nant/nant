f0729f98bb8e9844b602cfa9fb9fadadc5ca0512
drieseng <gert.driesen@telenet.be>
2003-10-16 07:33:27 +0000

    reworked changes by Scott Ford (sford at RJKTECH.com)

--- src/NAnt.VSNet/VcConfiguration.cs
+++ src/NAnt.VSNet/VcConfiguration.cs
@@ -23,45 +23,110 @@ using System.Collections.Specialized;
 using System.Globalization;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.IO;
+
+using NAnt.Core.Util;
 
 namespace NAnt.VSNet {
     /// <summary>
     /// A single build configuration for a Visual C++ project or for a specific
     /// file in the project.
     /// </summary>
-    internal class VcConfiguration {
+    public class VcConfiguration : ConfigurationBase {
         #region Internal Instance Constructors
 
-        internal VcConfiguration(XmlElement elem): this(elem, null) {
+        internal VcConfiguration(XmlElement elem, VcProject parentProject, Solution parentSln, string outputDir): this(elem, parentProject, parentSln, null, outputDir) {
         }
 
-        internal VcConfiguration(XmlElement elem, VcConfiguration parent) {
+        internal VcConfiguration(XmlElement elem, VcProject parentProject, Solution parentSln, VcConfiguration parent, string outputDir) {
+            string projectDir = Path.GetDirectoryName(parentSln.GetProjectFileFromGUID(parentProject.GUID));
+
             _parent = parent;
             _name = elem.GetAttribute("Name");
-            _outputDir = elem.GetAttribute("OutputDirectory");
+
+            if (StringUtils.IsNullOrEmpty(outputDir)) {
+                _outputPath = elem.GetAttribute("OutputDirectory");
+                _outputPath = new DirectoryInfo(Path.Combine(projectDir, elem.GetAttribute("OutputDirectory"))).FullName;
+            } else {
+                _outputPath = outputDir;
+            }
+
             _intermediateDir = elem.GetAttribute("IntermediateDirectory");
+            
+            string managedExtentions = elem.GetAttribute("ManagedExtensions");
+            if (managedExtentions != null) {
+                _managedExtensions = managedExtentions.Trim().ToUpper(CultureInfo.InvariantCulture) == "TRUE";
+            }
 
             if (String.Compare(elem.GetAttribute("WholeProgramOptimization"), "TRUE", true, CultureInfo.InvariantCulture) == 0) {
                 _wholeProgramOptimization = true;
             } 
 
             _htMacros = CollectionsUtil.CreateCaseInsensitiveHashtable();
-            _htMacros ["OutDir"] = _outputDir;
+            _htMacros ["OutDir"] = OutputPath;
+            _htMacros ["IntDir"] = _intermediateDir;
+            _htMacros ["ConfigurationName"] = Name;
+            _htMacros ["PlatformName"] = PlatformName;
+            _htMacros ["SolutionDir"] = Path.GetDirectoryName(parentSln.FileName);
+            _htMacros ["SolutionPath"] = parentSln.FileName;
+            _htMacros ["SolutionExt"] = Path.GetExtension(parentSln.FileName);
+            _htMacros ["ProjectDir"] = projectDir;
+  
             _rxMacro = new Regex(@"\$\((\w+)\)");
 
             _htTools = CollectionsUtil.CreateCaseInsensitiveHashtable();
 
             XmlNodeList tools = elem.GetElementsByTagName("Tool");
             foreach(XmlElement toolElem in tools) {
-            string toolName = toolElem.GetAttribute("Name");
-            Hashtable htToolSettings = CollectionsUtil.CreateCaseInsensitiveHashtable();
-            foreach(XmlAttribute attr in toolElem.Attributes) {                    if (attr.Name != "Name") {                        htToolSettings [attr.Name] = attr.Value;                    }                }                _htTools [toolName] = htToolSettings;            }        }
+                string toolName = toolElem.GetAttribute("Name");
+                Hashtable htToolSettings = CollectionsUtil.CreateCaseInsensitiveHashtable();
+                foreach(XmlAttribute attr in toolElem.Attributes) {                    if (attr.Name != "Name") {                        htToolSettings [attr.Name] = attr.Value;                    }                }                _htTools [toolName] = htToolSettings;            }            _targetPath = ExpandMacros(GetToolSetting("VCLinkerTool", "OutputFile"));            _htMacros ["TargetPath"] = _targetPath;            _htMacros ["TargetName"] = Path.GetFileNameWithoutExtension(Path.GetFileName(_targetPath));
+            _htMacros ["TargetExt"] = Path.GetExtension(_targetPath);
+
+            // create the output path if it doesn't already exist
+            Directory.CreateDirectory(OutputPath);
+        }
 
         #endregion Internal Instance Constructors
-        #region Internal Instance Properties        internal string Name {            get {                int index = _name.IndexOf("|");                if (index >= 0) {                    return _name.Substring(0, index);
+        #region Override implementation of ConfigurationBase        public override string OutputPath {
+            get { return _outputPath; }
+        }
+
+        public override string OutputFile {
+            get { 
+                string linkOutput = GetToolSetting("VCLinkerTool", "OutputFile");
+                if (linkOutput != null) {
+                    return Path.Combine(OutputPath, linkOutput);
+                }
+            
+                return Path.Combine(OutputPath, GetToolSetting("VCLibrarianTool", "OutputFile"));
+            }
+        }
+        #endregion Override implementation of ConfigurationBase        #region Public Instance Properties        public string ProjectDir {
+            get { return (string) _htMacros["ProjectDir"]; }
+        }
+        /// <summary>
+        /// Gets a value indicating whether Managed Extensions for C++ are 
+        /// enabled.
+        /// </summary>        public bool ManagedExtensions {            get { return _managedExtensions; }        }        #endregion Public Instance Properties        #region Internal Instance Properties        internal string Name {            get {                int index = _name.IndexOf("|");                if (index >= 0) {                    return _name.Substring(0, index);
                 }
                 else {
                     return _name;
+                }
+            }
+        }
+
+        internal string PlatformName {
+            get {
+                int index = _name.IndexOf("|");
+                if (index >= 0) {
+                    if (index < _name.Length) {
+                        return _name.Substring(index + 1, _name.Length - 1 - index);
+                    } else {
+                        return "";
+                    }
+                } else {
+                    return "";
                 }
             }
         }
@@ -74,7 +139,7 @@ namespace NAnt.VSNet {
                 }            }            if (_parent != null) {                return _parent.GetToolSetting(toolName, settingName);            }            return null;        }
         internal string[] GetToolArguments(string toolName, VcArgumentMap argMap) {            ArrayList args = new ArrayList();            Hashtable toolSettings = (Hashtable) _htTools [toolName];            if (toolSettings != null) {                foreach(DictionaryEntry de in toolSettings) {                    string arg = argMap.GetArgument((string) de.Key, ExpandMacros((string) de.Value));                    if (arg != null) {                        args.Add(arg);
                     }                }            }            return (string[]) args.ToArray(typeof(string));        }
-        internal string ExpandMacros(string s) {            return _rxMacro.Replace(s, new MatchEvaluator(EvaluateMacro));        }
+        internal string ExpandMacros(string s) {            if (s == null) {                return s;            }            return _rxMacro.Replace(s, new MatchEvaluator(EvaluateMacro));        }
 
         #endregion Internal Instance Methods
 
@@ -83,8 +148,9 @@ namespace NAnt.VSNet {
             }            return m.Value;        }
 
         #endregion Private Instance Methods
-        #region Private Instance Fields
-        private string          _name;        private VcConfiguration _parent;        private Hashtable       _htTools;        private string          _outputDir;        private string          _intermediateDir;        private Hashtable       _htMacros;        private Regex           _rxMacro;        private bool            _wholeProgramOptimization = false;
+        #region Private Instance Fields
+        private string _name;        private VcConfiguration _parent;        private Hashtable _htTools;        private string _outputPath;        private string _intermediateDir;        private string _targetPath;        private Hashtable _htMacros;        private Regex _rxMacro;        private bool _wholeProgramOptimization = false;
+        private bool _managedExtensions = false;
         #endregion Private Instance Fields
     }
 }
