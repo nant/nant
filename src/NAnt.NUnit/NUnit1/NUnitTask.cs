6e9c6e126aa958d98ec00637cc3357e7d5662214
drieseng <gert.driesen@telenet.be>
2003-12-04 20:31:57 +0000

    correction to make task doc xhtml compliant

--- src/NAnt.NUnit/NUnit1/NUnitTask.cs
+++ src/NAnt.NUnit/NUnit1/NUnitTask.cs
@@ -34,7 +34,7 @@ namespace NAnt.NUnit1.Tasks {
     /// </summary>
     /// <remarks>
     ///   <para>
-    ///   See the <a href="http://nunit.sf.net">NUnit home page</a> for more 
+    ///   See the <see href="http://nunit.sf.net">NUnit home page</see> for more 
     ///   information.
     ///   </para>
     ///   <para>
