4465438919f69643577271eee210eb858b98e465
drieseng <gert.driesen@telenet.be>
2003-12-18 17:37:02 +0000

    renamed CustomFunction* to Function*

--- src/NAnt.Core/Attributes/FunctionAttribute.cs
+++ src/NAnt.Core/Attributes/FunctionAttribute.cs
@@ -1,5 +1,5 @@
 // NAnt - A .NET build tool
-// Copyright (C) 2001 Gerry Shaw
+// Copyright (C) 2001-2003 Gerry Shaw
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,29 +15,31 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-// Gerry Shaw (gerry_shaw@yahoo.com)
+// Jaroslaw Kowalski (jkowalski@users.sourceforge.net)
 
 using System;
 
 namespace NAnt.Core.Attributes {
     /// <summary>
-    /// Indicates that class should be treated as a task.
+    /// Indicates that the method should be exposed as a function in NAnt build 
+    /// files.
     /// </summary>
     /// <remarks>
-    /// Attach this attribute to a subclass of CustomFunctionSetBase to have NAnt be able
-    /// to recognize it. 
+    /// Attach this attribute to a method of a class that derives from 
+    /// <see cref="FunctionSetBase" /> to have NAnt be able to recognize it.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Method, Inherited=false, AllowMultiple=false)]
-    public sealed class CustomFunctionAttribute : Attribute {
+    public sealed class FunctionAttribute : Attribute {
         #region Public Instance Constructors
-     
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="TaskNameAttribute" /> class
-        /// with the specified name.
+        /// Initializes a new instance of the <see cref="FunctionAttribute" />
+        /// class with the specified name.
         /// </summary>
-        /// <param name="name">The name of the task.</param>       
-        /// <exception cref="ArgumentNullException"><paramref name="name" /> is <see langword="null" />.</exception>       
-        public CustomFunctionAttribute(string name ) {
+        /// <param name="name">The name of the function.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="name" /> is <see langword="null" />.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="name" /> is a zero-length <see cref="string" />.</exception>
+        public FunctionAttribute(string name) {
             if (name == null) {
                 throw new ArgumentNullException("name");
             }
@@ -45,29 +47,31 @@ namespace NAnt.Core.Attributes {
             if (name.Trim().Length == 0) {
                 throw new ArgumentOutOfRangeException("name", name, "A zero-length string is not an allowed value.");
             }
+
             _name = name;
         }
+
         #endregion Public Instance Constructors
          
-        #region Private Instance Fields
-
-        private string _name;
-        
-        #endregion Private Instance Fields
-               
         #region Public Instance Properties
 
         /// <summary>
-        /// Gets or sets the name of the task.
+        /// Gets or sets the name of the function.
         /// </summary>
         /// <value>
-        /// The name of the task.
+        /// The name of the function.
         /// </value>
         public string Name {
             get { return _name; }
             set { _name = value; }
         }
-                
-        #endregion Public Instance Properties       
+
+        #endregion Public Instance Properties
+
+        #region Private Instance Fields
+
+        private string _name;
+        
+        #endregion Private Instance Fields
     }
 }
--- src/NAnt.Core/Attributes/FunctionSetAttribute.cs
+++ src/NAnt.Core/Attributes/FunctionSetAttribute.cs
@@ -1,5 +1,5 @@
 // NAnt - A .NET build tool
-// Copyright (C) 2001 Gerry Shaw
+// Copyright (C) 2001-2003 Gerry Shaw
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,42 +15,55 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-// Gerry Shaw (gerry_shaw@yahoo.com)
+// Jaroslaw Kowalski (jkowalski@users.sourceforge.net)
 
 using System;
 
 namespace NAnt.Core.Attributes {
     /// <summary>
-    /// Indicates that class should be treated as a task.
+    /// Indicates that class should be treated as a set of functions.
     /// </summary>
     /// <remarks>
-    /// Attach this attribute to a subclass of CustomFunctionSetBase to have NAnt be able
-    /// to recognize it as containing custom functions
+    /// Attach this attribute to a class that derives from <see cref="FunctionSetBase" /> 
+    /// to have NAnt be able to recognize it as containing custom functions.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Class, Inherited=false, AllowMultiple=false)]
-    public sealed class CustomFunctionSetAttribute : Attribute {
+    public sealed class FunctionSetAttribute : Attribute {
         #region Public Instance Constructors
 
-        // TODO remove this
-        public CustomFunctionSetAttribute( )  {            
-        }
         /// <summary>
-        /// Initializes a new instance of the <see cref="CustomFunctionSetAttribute" /> class
-        /// with the specified name.
+        /// Initializes a new instance of the <see cref="FunctionSetAttribute" /> 
+        /// class with the specified name.
         /// </summary>
-        /// <param name="prefix">The name of the task.</param>
-        /// <param name="namespaceuri">The name of the task.</param>
-        /// <exception cref="ArgumentNullException"><paramref name="name" /> is <see langword="null" />.</exception>
-        /// <exception cref="ArgumentOutOfRangeException"><paramref name="name" /> is a zero-length <see cref="string" />.</exception>
-        public CustomFunctionSetAttribute(string prefix, string namespaceuri ) {
+        /// <param name="prefix">The prefix used to distinguish the functions.</param>
+        /// <param name="category">The category of the functions.</param>
+        /// <exception cref="ArgumentNullException">
+        ///   <para><paramref name="prefix" /> is <see langword="null" />.</para>
+        ///   <para>-or-</para>
+        ///   <para><paramref name="category" /> is <see langword="null" />.</para>
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <para><paramref name="prefix" /> is a zero-length <see cref="string" />.</para>
+        ///   <para>-or-</para>
+        ///   <para><paramref name="category" /> is a zero-length <see cref="string" />.</para>
+        /// </exception>
+        public FunctionSetAttribute(string prefix, string category) {
             if (prefix == null) {
                 throw new ArgumentNullException("prefix");
+            }
+            if (category == null) {
+                throw new ArgumentNullException("category");
             }
 
             if (prefix.Trim().Length == 0) {
                 throw new ArgumentOutOfRangeException("prefix", prefix, "A zero-length string is not an allowed value.");
             }
+            if (category.Trim().Length == 0) {
+                throw new ArgumentOutOfRangeException("category", category, "A zero-length string is not an allowed value.");
+            }
+
             _prefix = prefix;
+            _category = category;
         }
 
         #endregion Public Instance Constructors
@@ -58,11 +71,14 @@ namespace NAnt.Core.Attributes {
         #region Public Instance Properties
 
         /// <summary>
-        /// Gets or sets the category of the function set. This will be displayed in userdoc.
+        /// Gets or sets the category of the function set.
         /// </summary>
         /// <value>
-        /// The name of the category
+        /// The name of the category of the function set.
         /// </value>
+        /// <remarks>
+        /// This will be displayed in the user docs.
+        /// </remarks>
         public string Category {
             get { return _category; }
             set { _category = value; }
@@ -72,32 +88,19 @@ namespace NAnt.Core.Attributes {
         /// Gets or sets the prefix of all functions in this function set.
         /// </summary>
         /// <value>
-        /// The prefix.
+        /// The prefix of the functions in this function set.
         /// </value>
         public string Prefix {
             get { return _prefix; }
             set { _prefix = value; }
         }
         
-        /// <summary>
-        /// Gets or sets the namespace URI of the functon set.
-        /// </summary>
-        /// <value>
-        /// The namespace URI.
-        /// </value>
-        public string NamespaceUri 
-        {
-            get { return _namespaceUri; }
-            set { _namespaceUri = value; }
-        }
-
         #endregion Public Instance Properties
 
         #region Private Instance Fields
 
         private string _prefix;
         private string _category;
-        private string _namespaceUri;        
 
         #endregion Private Instance Fields
     }
