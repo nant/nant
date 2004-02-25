7680b43b4b0273a81680a149247ba540c21655a7
drieseng <gert.driesen@telenet.be>
2004-02-25 17:07:27 +0000

    added testcase for xmlpeek namespace support

--- tests/NAnt.Core/Tasks/XmlPeekTest.cs
+++ tests/NAnt.Core/Tasks/XmlPeekTest.cs
@@ -38,13 +38,30 @@ namespace Tests.NAnt.Core.Tasks {
                 + "<echo message=\"configuration.server={1}\" />"
             + "</project>";
 
+        private const string _projectXmlWithNamespace = "<?xml version=\"1.0\"?>"
+            + "<project>"
+                + "<xmlpeek {0} property=\"configuration.server\">"
+                    + "<namespaces>"
+                        + "<namespace prefix=\"x\" uri=\"http://www.gordic.cz/shared/project-config/v_1.0.0.0\" />"
+                    + "</namespaces>"
+                + "</xmlpeek>"
+                + "<echo message=\"configuration.server={1}\" />"
+            + "</project>";
+
         private const string _validXml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" 
             + "<configuration>" 
                 + "<appSettings>"
                     + "<add key=\"server\" value=\"testhost.somecompany.com\" />"
                 + "</appSettings>"
             + "</configuration>";
-       
+
+        private const string _validXmlWithNamespace = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+            + "<configuration xmlns=\"http://www.gordic.cz/shared/project-config/v_1.0.0.0\">"
+                + "<appSettings>"
+                    + "<add key=\"server\" value=\"testhost.somecompany.com\" />"
+                + "</appSettings>"
+            + "</configuration>";
+
         #endregion Private Instance Fields
 
         #region Public Instance Methods
@@ -56,11 +73,28 @@ namespace Tests.NAnt.Core.Tasks {
 
             // set-up task attributes
             string taskAttributes = string.Format(CultureInfo.InvariantCulture,
-                "file=\"{0}\" xpath=\"/configuration/appSettings/add[@key ='server']/@value\"",
+                "file=\"{0}\" xpath=\"/configuration/appSettings/add[@key='server']/@value\"",
                 xmlFile);
 
             // execute build
             string buildLog = RunBuild(string.Format(CultureInfo.InvariantCulture, _projectXml,
+                taskAttributes, "${configuration.server}"));
+
+            // ensure the correct node was read
+            Assertion.Assert("Invalid node was retrieved.", buildLog.IndexOf("configuration.server=testhost.somecompany.com") != -1);
+        }
+
+        [Test]
+        public void Test_PeekValidXmlWithNamespace() {
+            // write xml content to file
+            string xmlFile = TempFile.CreateWithContents(_validXmlWithNamespace);
+
+            // set-up task attributes
+            string taskAttributes = string.Format(CultureInfo.InvariantCulture,
+                "file=\"{0}\" xpath=\"/x:configuration/x:appSettings/x:add[@key='server']/@value\"", xmlFile);
+
+            // execute build
+            string buildLog = RunBuild(string.Format(CultureInfo.InvariantCulture, _projectXmlWithNamespace,
                 taskAttributes, "${configuration.server}"));
 
             // ensure the correct node was read
