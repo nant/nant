e356cc043f6e2bb7aa35d465c26afaa6827c5e87
drieseng <gert.driesen@telenet.be>
2004-02-27 13:17:18 +0000

    add CharacterSet enum for specifying the characters the c+ compiler should use

---
+++ src/NAnt.VisualCpp/Types/CharacterSet.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace NAnt.VisualCpp.Types {
+    /// <summary>
+    /// Defines the character sets that can be used by the C++ compiler.
+    /// </summary>
+    public enum CharacterSet {
+        /// <summary>
+        /// Have the compiler determine the character set.
+        /// </summary>
+        NotSet = 0,
+
+        /// <summary>
+        /// Unicode character set.
+        /// </summary>
+        Unicode = 1,
+
+        /// <summary>
+        /// Multi-byte character set.
+        /// </summary>
+        MultiByte = 2
+    }
+}
