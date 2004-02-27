2085a463c76a9842f51a01ee69b67678ac342209
drieseng <gert.driesen@telenet.be>
2004-02-27 13:20:12 +0000

    added support for character set argument, allow file configs to override settings of parent

--- src/NAnt.VSNet/VcConfiguration.cs
+++ src/NAnt.VSNet/VcConfiguration.cs
@@ -29,6 +29,8 @@ using System.Xml;
 
 using NAnt.Core.Util;
 
+using NAnt.VisualCpp.Types;
+
 namespace NAnt.VSNet {
     /// <summary>
     /// A single build configuration for a Visual C++ project or for a specific
@@ -40,17 +42,16 @@ namespace NAnt.VSNet {
         internal VcConfiguration(XmlElement elem, VcProject parentProject, Solution parentSln, DirectoryInfo outputDir) : this(elem, parentProject, parentSln, null, outputDir) {
         }
 
-        internal VcConfiguration(XmlElement elem, VcProject parentProject, Solution parentSln, VcConfiguration parent, DirectoryInfo outputDir) {
+        internal VcConfiguration(XmlElement elem, VcProject parentProject, Solution parentSln, VcConfiguration parentConfig, DirectoryInfo outputDir) {
             DirectoryInfo projectDir = new DirectoryInfo(Path.GetDirectoryName(
                 parentSln.GetProjectFileFromGuid(parentProject.Guid)));
 
-            _parent = parent;
+            _parentConfig = parentConfig;
 
             // get name of configuration (also contains the targeted platform)
             _name = elem.GetAttribute("Name");
 
             // initialize variables for usage in macros
-            _htMacros = CollectionsUtil.CreateCaseInsensitiveHashtable();
             _htMacros["ProjectName"] = parentProject.Name;
             _htMacros["ConfigurationName"] = Name;
             _htMacros["PlatformName"] = PlatformName;
@@ -61,25 +62,52 @@ namespace NAnt.VSNet {
 
             // determine output directory
             if (outputDir == null) {
-                _outputDir = new DirectoryInfo(Path.Combine(projectDir.FullName, 
-                    ExpandMacros(elem.GetAttribute("OutputDirectory"))));
+                XmlAttribute outputDirAttribute = elem.Attributes["OutputDirectory"];
+                if (outputDirAttribute != null) {
+                    _outputDir = new DirectoryInfo(Path.Combine(projectDir.FullName, 
+                        ExpandMacros(outputDirAttribute.Value)));
+                } else if (_parentConfig != null) {
+                    _outputDir = _parentConfig.OutputDir;
+                } else {
+                    // TO-DO : throw BuildException, as there's no output directory defined
+                    // or do some configuration types no require this ?
+                }
             } else {
                 _outputDir = outputDir;
             }
             // make output directory available in macros
             _htMacros["OutDir"] = OutputDir.FullName;
 
-            string managedExtentions = elem.GetAttribute("ManagedExtensions");
+            string managedExtentions = GetXmlAttributeValue(elem, "ManagedExtensions");
             if (managedExtentions != null) {
                 _managedExtensions = managedExtentions.Trim().ToUpper(CultureInfo.InvariantCulture) == "TRUE";
+            } else if (_parentConfig != null) {
+                _managedExtensions = _parentConfig.ManagedExtensions;
             }
 
-            if (String.Compare(elem.GetAttribute("WholeProgramOptimization"), "TRUE", true, CultureInfo.InvariantCulture) == 0) {
-                _wholeProgramOptimization = true;
-            } 
+            string wholeProgramOptimization = GetXmlAttributeValue(elem, "WholeProgramOptimization");
+            if (wholeProgramOptimization != null) {
+                _wholeProgramOptimization = wholeProgramOptimization.Trim().ToUpper(CultureInfo.InvariantCulture) == "TRUE";
+            } else if (_parentConfig != null) {
+                _wholeProgramOptimization = _parentConfig.WholeProgramOptimization;
+            }
+
+            string characterSet = GetXmlAttributeValue(elem, "CharacterSet");
+            if (characterSet != null) {
+                _characterSet = (CharacterSet) Enum.ToObject(typeof(CharacterSet), 
+                    int.Parse(characterSet, CultureInfo.InvariantCulture));
+            } else if (_parentConfig != null) {
+                _characterSet = _parentConfig.CharacterSet;
+            }
 
             // get intermediate directory and expand macros 
-            _intermediateDir = ExpandMacros(elem.GetAttribute("IntermediateDirectory"));
+            XmlAttribute intermidiateDirAttribute = elem.Attributes["IntermediateDirectory"];
+            if (intermidiateDirAttribute != null) {
+                _intermediateDir = ExpandMacros(intermidiateDirAttribute.Value);
+            } else if (_parentConfig != null) {
+                _intermediateDir = _parentConfig.IntermediateDir;
+            }
+
             // make intermediate directory available in macros
             _htMacros["IntDir"] = _intermediateDir;
 
@@ -89,7 +117,7 @@ namespace NAnt.VSNet {
             foreach(XmlElement toolElem in tools) {
                 string toolName = toolElem.GetAttribute("Name");
                 Hashtable htToolSettings = CollectionsUtil.CreateCaseInsensitiveHashtable();
-                foreach(XmlAttribute attr in toolElem.Attributes) {                    if (attr.Name != "Name") {                        htToolSettings[attr.Name] = attr.Value;                    }                }                _htTools[toolName] = htToolSettings;            }            _targetPath = ExpandMacros(GetToolSetting("VCLinkerTool", "OutputFile"));            _htMacros ["TargetPath"] = _targetPath;            _htMacros ["TargetName"] = Path.GetFileNameWithoutExtension(Path.GetFileName(_targetPath));
+                foreach(XmlAttribute attr in toolElem.Attributes) {                    if (attr.Name != "Name") {                        htToolSettings[attr.Name] = attr.Value;                    }                }                _htTools[toolName] = htToolSettings;            }            _targetPath = ExpandMacros(GetToolSetting("VCLinkerTool", "OutputFile"));            _htMacros ["TargetPath"] = _targetPath;            _htMacros ["TargetName"] = Path.GetFileNameWithoutExtension(Path.GetFileName(_targetPath));
             _htMacros ["TargetExt"] = Path.GetExtension(_targetPath);
 
             // create the output path if it doesn't already exist
@@ -115,6 +143,8 @@ namespace NAnt.VSNet {
             get { return new DirectoryInfo((string) _htMacros["ProjectDir"]); }
         }
         /// <summary>
+        /// Tells the compiler which character set to use.
+        /// </summary>        public CharacterSet CharacterSet {            get { return _characterSet; }        }        /// <summary>
         /// Gets a value indicating whether Managed Extensions for C++ are 
         /// enabled.
         /// </summary>        public bool ManagedExtensions {            get { return _managedExtensions; }        }        #endregion Public Instance Properties        #region Internal Instance Properties        /// <summary>
@@ -152,24 +182,55 @@ namespace NAnt.VSNet {
         /// Intermediate directory, specified relative to project directory.
         /// </summary>        internal string IntermediateDir {            get { return _intermediateDir; }        }
         internal bool WholeProgramOptimization {            get { return _wholeProgramOptimization; }        }
+
+        /// <summary>
+        /// Gets the collection of macros that can be expanded in configuration
+        /// settings.
+        /// </summary>
+        internal Hashtable Macros {
+            get { return _htMacros; }
+        }
         #endregion Internal Instance Properties
-        #region Internal Instance Methods
-        internal string GetToolSetting(string toolName, string settingName) {            Hashtable toolSettings = (Hashtable) _htTools [toolName];            if (toolSettings != null) {                string setting = (string) toolSettings [settingName];                if (setting != null) {                    return ExpandMacros(setting);
-                }            }            if (_parent != null) {                return _parent.GetToolSetting(toolName, settingName);            }            return null;        }
-        internal string[] GetToolArguments(string toolName, VcArgumentMap argMap) {            ArrayList args = new ArrayList();            Hashtable toolSettings = (Hashtable) _htTools [toolName];            if (toolSettings != null) {                foreach(DictionaryEntry de in toolSettings) {                    string arg = argMap.GetArgument((string) de.Key, ExpandMacros((string) de.Value));                    if (arg != null) {                        args.Add(arg);
-                    }                }            }            return (string[]) args.ToArray(typeof(string));        }
+        #region Private Static Methods        /// <summary>
+        /// Gets the value of the specified attribute from the specified node.
+        /// </summary>
+        /// <param name="xmlNode">The node of which the attribute value should be retrieved.</param>
+        /// <param name="attributeName">The attribute of which the value should be returned.</param>
+        /// <returns>
+        /// The value of the attribute with the specified name or <see langword="null" />
+        /// if the attribute does not exist or has no value.
+        /// </returns>
+        private static string GetXmlAttributeValue(XmlNode xmlNode, string attributeName) {
+            string attributeValue = null;
+
+            if (xmlNode != null) {
+                XmlAttribute xmlAttribute = (XmlAttribute) xmlNode.Attributes.GetNamedItem(attributeName);
+
+                if (xmlAttribute != null) {
+                    attributeValue = StringUtils.ConvertEmptyToNull(xmlAttribute.Value);
+                }
+            }
+
+            return attributeValue;
+        }
+        #endregion Private Static Methods        #region Internal Instance Methods
+        internal string GetToolSetting(string toolName, string settingName) {            Hashtable toolSettings = (Hashtable) _htTools[toolName];            if (toolSettings != null) {                string setting = (string) toolSettings[settingName];                if (setting != null) {                    return ExpandMacros(setting);
+                }            }            if (_parentConfig != null) {                return _parentConfig.GetToolSetting(toolName, settingName);            }            return null;        }
+        internal Hashtable GetToolArguments(string toolName, VcArgumentMap argMap) {            Hashtable args;            if (_parentConfig != null) {                args = _parentConfig.GetToolArguments(toolName, argMap);            } else {                args = CollectionsUtil.CreateCaseInsensitiveHashtable();            }            Hashtable toolSettings = (Hashtable) _htTools[toolName];            if (toolSettings != null) {                foreach (DictionaryEntry de in toolSettings) {                    string arg = argMap.GetArgument((string) de.Key, ExpandMacros((string) de.Value));                    if (arg != null) {                        args[(string) de.Key] = arg;
+                    }                }            }            return args;        }
         internal string ExpandMacros(string s) {            if (s == null) {                return s;            }            return _rxMacro.Replace(s, new MatchEvaluator(EvaluateMacro));        }
 
         #endregion Internal Instance Methods
 
         #region Private Instance Methods
-        private string EvaluateMacro(Match m) {            string macroValue = (string) _htMacros [m.Groups [1].Value];            if (macroValue != null) {                return macroValue;
+        private string EvaluateMacro(Match m) {            string macroValue = (string) _htMacros[m.Groups[1].Value];            if (macroValue != null) {                return macroValue;
             }            return m.Value;        }
 
         #endregion Private Instance Methods
         #region Private Instance Fields
-        private readonly string _name;        private VcConfiguration _parent;        private Hashtable _htTools;        private readonly DirectoryInfo _outputDir;        private readonly string _intermediateDir;        private readonly string _targetPath;        private Hashtable _htMacros;        private readonly Regex _rxMacro = new Regex(@"\$\((\w+)\)");        private readonly bool _wholeProgramOptimization = false;
-        private readonly bool _managedExtensions = false;
+        private readonly string _name;        private VcConfiguration _parentConfig;        private Hashtable _htTools;        private readonly DirectoryInfo _outputDir;        private readonly string _intermediateDir;        private readonly string _targetPath;        private Hashtable _htMacros = CollectionsUtil.CreateCaseInsensitiveHashtable();        private readonly Regex _rxMacro = new Regex(@"\$\((\w+)\)");        private readonly bool _wholeProgramOptimization;
+        private readonly bool _managedExtensions;
+        private readonly CharacterSet _characterSet = CharacterSet.NotSet;
         #endregion Private Instance Fields
     }
 }
