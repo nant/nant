fd0b924c2f77362fd082e1e67ca596325584c270
drieseng <gert.driesen@telenet.be>
2004-03-20 17:58:44 +0000

    added test for namespace support

--- tests/NAnt.Core/Tasks/XmlPokeTest.cs
+++ tests/NAnt.Core/Tasks/XmlPokeTest.cs
@@ -39,13 +39,35 @@ namespace Tests.NAnt.Core.Tasks {
                 + "<echo message=\"configuration.server={2}!\" />"
             + "</project>";
 
+        private const string _projectXmlWithNamespace = "<?xml version=\"1.0\"?>"
+            + "<project>"
+                + "<xmlpoke {0}>"
+                    + "<namespaces>"
+                        + "<namespace prefix=\"x\" uri=\"http://www.gordic.cz/shared/project-config/v_1.0.0.0\" />"
+                    + "</namespaces>"
+                + "</xmlpoke>"
+                + "<xmlpeek {1} property=\"configuration.server\">" 
+                    + "<namespaces>"
+                        + "<namespace prefix=\"x\" uri=\"http://www.gordic.cz/shared/project-config/v_1.0.0.0\" />"
+                    + "</namespaces>"
+                + "</xmlpeek>" 
+                + "<echo message=\"configuration.server={2}!\" />"
+            + "</project>";
+
         private const string _validXml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" 
             + "<configuration>" 
                 + "<appSettings>"
                     + "<add key=\"server\" value=\"testhost.somecompany.com\" />"
                 + "</appSettings>"
             + "</configuration>";
-       
+
+        private const string _validXmlWithNamespace = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+            + "<configuration xmlns=\"http://www.gordic.cz/shared/project-config/v_1.0.0.0\">"
+                + "<appSettings>"
+                    + "<add key=\"server\" value=\"testhost.somecompany.com\" />"
+                + "</appSettings>"
+            + "</configuration>";
+
         #endregion Private Instance Fields
 
         #region Public Instance Methods
@@ -68,6 +90,35 @@ namespace Tests.NAnt.Core.Tasks {
             // execute build
             string buildLog = RunBuild(string.Format(CultureInfo.InvariantCulture, 
                 _projectXml, xmlPokeTaskAttributes, xmlPeekTaskAttributes, 
+                "${configuration.server}"));
+
+            // ensure original value was not retained
+            Assertion.Assert("Value of node was not updated, orignal value is still in xml file.", 
+                buildLog.IndexOf("configuration.server=testhost.somecompany.com!") == -1);
+
+            // ensure new value was set
+            Assertion.Assert("Value of node was not updated correctly, new value does not match.", 
+                buildLog.IndexOf("configuration.server=productionhost.somecompany.com!") != -1);
+        }
+
+        [Test]
+        public void Test_PokeValidXmlWithNamespace() {
+            // write xml content to file
+            string xmlFile = TempFile.CreateWithContents(_validXmlWithNamespace);
+
+            // set-up <xmlpoke> task attributes
+            string xmlPokeTaskAttributes = string.Format(CultureInfo.InvariantCulture,
+                "file=\"{0}\" xpath=\"/x:configuration/x:appSettings/x:add[@key ='server']/@value\"" +
+                " value=\"productionhost.somecompany.com\"", xmlFile);
+
+            // set-up <xmlpeek> task attributes
+            string xmlPeekTaskAttributes = string.Format(CultureInfo.InvariantCulture,
+                "file=\"{0}\" xpath=\"/x:configuration/x:appSettings/x:add[@key ='server']/@value\"",
+                xmlFile);
+
+            // execute build
+            string buildLog = RunBuild(string.Format(CultureInfo.InvariantCulture, 
+                _projectXmlWithNamespace, xmlPokeTaskAttributes, xmlPeekTaskAttributes, 
                 "${configuration.server}"));
 
             // ensure original value was not retained
