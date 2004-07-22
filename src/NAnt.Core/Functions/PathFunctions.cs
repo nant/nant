602a65e42bf2edb81487af12066beb6ba8648c7b
drieseng <gert.driesen@telenet.be>
2004-07-22 04:54:32 +0000

    prevent returning null values, updated docs

--- src/NAnt.Core/Functions/PathFunctions.cs
+++ src/NAnt.Core/Functions/PathFunctions.cs
@@ -25,8 +25,9 @@ using System.Reflection;
 using System.Globalization;
 
 using NAnt.Core;
-using NAnt.Core.Types;
 using NAnt.Core.Attributes;
+using NAnt.Core.Types;
+using NAnt.Core.Util;
 
 namespace NAnt.Core.Functions {
     [FunctionSet("path", "Path")]
@@ -44,7 +45,10 @@ namespace NAnt.Core.Functions {
         /// Returns the fully qualified path.
         /// </summary>
         /// <param name="path">The file or directory for which to obtain absolute path information.</param>
-        /// <returns>A string containing the fully qualified location of path, such as "C:\MyFile.txt".</returns>
+        /// <returns>
+        /// A string containing the fully qualified location of <paramref name="path" />,
+        /// such as "C:\MyFile.txt".
+        /// </returns>
         [Function("get-full-path")]
         public string GetFullPath(string path) {
             return Project.GetFullPath(path);
@@ -77,12 +81,21 @@ namespace NAnt.Core.Functions {
         /// defined in <see cref="System.IO.Path.InvalidPathChars"/>InvalidPathChars.</param>
         /// <param name="extension">The new extension (with a leading period). Specify a null reference 
         /// to remove an existing extension from <paramref name="path" />.</param>
-        /// <returns>A string containing the modified path information.
-        /// <p>On Windows-based desktop platforms, if <paramref name="path" /> is a null reference or an empty <see cref="string" /> (""), 
-        /// the path information is returned unmodified. If extension is a null reference, the returned string 
-        /// contains the specified path with its extension removed. If <paramref name="path" /> has no extension, and extension is 
-        /// not a null reference, the returned path <see cref="string" /> contains extension appended to the end of <paramref name="path" />.</p></returns>
-        ///<remarks>for more information see the <see cref="System.IO.Path"/> Documentation</remarks>
+        /// <returns>
+        /// <para>
+        /// A string containing the modified path information.
+        /// </para>
+        /// <para>
+        /// On Windows-based desktop platforms, if <paramref name="path" /> is 
+        /// an empty <see cref="string" />, the path information is returned 
+        /// unmodified. If <paramref name="path" /> has no extension, the returned 
+        /// path <see cref="string" /> contains <paramref name="extension" /> 
+        /// appended to the end of <paramref name="path" />.
+        /// </para>
+        /// </returns>
+        ///<remarks>
+        ///For more information see the <see cref="System.IO.Path"/> documentation.
+        ///</remarks>
         [Function("change-extension")]
         public static string ChangeExtension(string path, string extension) {
             return Path.ChangeExtension(path, extension);
@@ -92,23 +105,28 @@ namespace NAnt.Core.Functions {
         /// Returns the directory information for the specified path string.
         /// </summary>
         /// <param name="path">The path of a file or directory.</param>
-        /// <returns>A <see cref="string" /> containing directory information for <paramref name="path" />, 
-        /// or a null reference if <paramref name="path" /> denotes a root directory, is the empty string (""), 
-        /// or is a null reference. 
-        /// Returns String.Empty if <paramref name="path" /> does not contain directory information.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing directory information for 
+        /// <paramref name="path" />, or an empty <see cref="string" /> if 
+        /// <paramref name="path" /> denotes a root directory, or does not
+        /// contain directory information.
+        /// </returns>
         [Function("get-directory-name")]
         public static string GetDirectoryName(string path) {
-            return Path.GetDirectoryName(path);
+            string dirName = Path.GetDirectoryName(path);
+            return StringUtils.ConvertNullToEmpty(dirName);
         }
 
         /// <summary>
         /// Returns the extension for the specified path string.
         /// </summary>
         /// <param name="path">The path string from which to get the extension.</param>
-        /// <returns>A <see cref="string" /> containing the extension of the specified <paramref name="path" /> 
-        /// (including the "."), a null reference, or Empty. If <paramref name="path" /> is a null 
-        /// reference, <see cref="GetExtension" /> returns a null reference. 
-        /// If <paramref name="path" /> does not have extension information, <see cref="GetExtension" /> returns Empty.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing the extension of the specified 
+        /// <paramref name="path" /> (including the "."), or an empty 
+        /// <see cref="string" /> if <paramref name="path" /> does not have 
+        /// extension information.
+        /// </returns>
         [Function("get-extension")]
         public static string GetExtension(string path) {
             return Path.GetExtension(path);
@@ -118,9 +136,16 @@ namespace NAnt.Core.Functions {
         /// Returns the filename for the specified path string.
         /// </summary>
         /// <param name="path">The path string from which to obtain the file name and extension.</param>
-        /// <returns>A <see cref="string" /> consisting of the characters after the last directory character in path. 
-        /// If the last character of <paramref name="path" /> is a directory or volume separator character, this method returns Empty. 
-        /// If <paramref name="path" /> is a null reference, this method returns a null reference.</returns>
+        /// <returns>
+        /// <para>
+        /// A <see cref="string" /> consisting of the characters after the last 
+        /// directory character in path. 
+        /// </para>
+        /// <para>
+        /// If the last character of <paramref name="path" /> is a directory or 
+        /// volume separator character, an empty <see cref="string" /> is returned.
+        /// </para>
+        /// </returns>
         [Function("get-file-name")]
         public static string GetFileName(string path) {
             return Path.GetFileName(path);
@@ -130,7 +155,11 @@ namespace NAnt.Core.Functions {
         /// Returns the filename without extension for the specified path string.
         /// </summary>
         /// <param name="path">The path of the file.</param>
-        /// <returns>A <see cref="string" /> containing the <see cref="string" /> returned by <see cref="GetFileName" />, minus the last period (.) and all characters following it.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing the <see cref="string" /> returned 
+        /// by <see cref="GetFileName" />, minus the last period (.) and all 
+        /// characters following it.
+        /// </returns>
         [Function("get-file-name-without-extension")]
         public static string GetFileNameWithoutExtension(string path) {
             return Path.GetFileNameWithoutExtension(path);
@@ -140,18 +169,23 @@ namespace NAnt.Core.Functions {
         /// Gets the root directory of the specified path.
         /// </summary>
         /// <param name="path">The path from which to obtain root directory information.</param>
-        /// <returns>A <see cref="string" /> containing the root directory of <paramref name="path" />, such as "C:\", or a null reference 
-        /// if <paramref name="path" /> is a null reference, or an empty <see cref="string" /> if <paramref name="path" /> does not contain 
-        /// root directory information.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing the root directory of 
+        /// <paramref name="path" />, such as "C:\", or an empty <see cref="string" /> 
+        /// if <paramref name="path" /> does not contain root directory information.
+        /// </returns>
         [Function("get-path-root")]
         public static string GetPathRoot(string path) {
-            return Path.GetPathRoot(path);
+            string pathRoot = Path.GetPathRoot(path);
+            return StringUtils.ConvertNullToEmpty(pathRoot);
         }
 
         /// <summary>
         /// Returns a uniquely named zero-byte temporary file on disk and returns the full path to that file.
         /// </summary>
-        /// <returns>A <see cref="string" /> containing the name of the temporary file.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing the name of the temporary file.
+        /// </returns>
         [Function("get-temp-file-name")]
         public static string GetTempFileName() {
             return Path.GetTempFileName();
@@ -160,7 +194,10 @@ namespace NAnt.Core.Functions {
         /// <summary>
         /// Gets the path to the temporary directory.
         /// </summary>
-        /// <returns>A <see cref="string" /> containing the path information of a temporary directory.</returns>
+        /// <returns>
+        /// A <see cref="string" /> containing the path information of a 
+        /// temporary directory.
+        /// </returns>
         [Function("get-temp-path")]
         public static string GetTempPath() {
             return Path.GetTempPath();
@@ -170,9 +207,12 @@ namespace NAnt.Core.Functions {
         /// Determines whether a path string includes an extension.
         /// </summary>
         /// <param name="path">The path to search for an extension.</param>
-        /// <returns><see langword="true" />. if the characters that follow the last directory separator (\\ or/) or 
-        /// volume separator (:) in the <paramref name="path" /> include a period (.) followed by one or more characters; 
-        /// otherwise, <see langword="false" />.</returns>
+        /// <returns>
+        /// <see langword="true" />. if the characters that follow the last 
+        /// directory separator or volume separator in the <paramref name="path" /> 
+        /// include a period (.) followed by one or more characters; 
+        /// otherwise, <see langword="false" />.
+        /// </returns>
         [Function("has-extension")]
         public static bool HasExtension(string path) {
             return Path.HasExtension(path);
@@ -182,7 +222,10 @@ namespace NAnt.Core.Functions {
         /// Determines whether a path string is absolute.
         /// </summary>
         /// <param name="path">The path to test.</param>
-        /// <returns><see langword="true" /> if path contains an absolute <paramref name="path" />; otherwise,  <see langword="false" />.</returns>
+        /// <returns>
+        /// <see langword="true" /> if path contains an absolute <paramref name="path" />; 
+        /// otherwise, <see langword="false" />.
+        /// </returns>
         [Function("is-path-rooted")]
         public static bool IsPathRooted(string path) {
             return Path.IsPathRooted(path);
