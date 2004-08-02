beb1b45c0e6721701592dd415ac5a678484d7255
drieseng <gert.driesen@telenet.be>
2004-08-02 11:45:43 +0000

    changed namespace, no longer use LogPrefix, formatting

--- examples/UserTask/UserTask.cs
+++ examples/UserTask/UserTask.cs
@@ -1,12 +1,16 @@
 using NAnt.Core;
 using NAnt.Core.Attributes;
 
-namespace NAnt.SampleTask {
-
+namespace NAnt.Examples.Tasks {
     [TaskName("usertask")]
     public class TestTask : Task {
+        #region Private Instance Fields
 
-        string _message = null;
+        private string _message;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
 
         [TaskAttribute("message", Required=true)]
         public string FileName {
@@ -14,8 +18,14 @@ namespace NAnt.SampleTask {
             set { _message = value; }
         }
 
+        #endregion Public Instance Properties
+
+        #region Override implementation of Task
+
         protected override void ExecuteTask() {
-            Log(Level.Info, LogPrefix + _message.ToUpper());
+            Log(Level.Info, Message.ToUpper());
         }
+
+        #endregion Override implementation of Task
     }
 }
