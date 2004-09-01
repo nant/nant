410a11ab57d109f782244298f357105f77b3d22f
drieseng <gert.driesen@telenet.be>
2004-09-01 19:03:33 +0000

    added fileset specific for <zip> task, and which adds support for fileset-level prefix attribute

---
+++ src/NAnt.Compression/Types/ZipFileSet.cs
@@ -0,0 +1,60 @@
+// NAnt - A .NET build tool
+// Copyright (C) 2003 Scott Hernandez
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Gert Driesen (gert.driesen@ardatis.com)
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using NAnt.Core.Types;
+using NAnt.Core.Util;
+
+using NAnt.Zip.Tasks;
+
+namespace NAnt.Zip.Types {
+    /// <summary>
+    /// A <see cref="ZipFileSet" /> is a <see cref="FileSet" /> with extra 
+    /// attributes useful in the context of the <see cref="ZipTask" />.
+    /// </summary>
+    [TaskName("zipfileset")]
+    public class ZipFileSet : FileSet {
+        #region Private Instance Fields
+
+        private string _prefix;
+
+        #endregion Private Instance Fields
+
+        #region Public Instance Properties
+
+        /// <summary>
+        /// The top level directory prefix. If set, all file and directory paths 
+        /// in the fileset will have this value prepended. Can either be a single 
+        /// directory name or a "/" separated path.
+        /// </summary>
+        [TaskAttribute("prefix", Required=false)]
+        public string Prefix {
+            get { return _prefix; }
+            set { 
+                _prefix = StringUtils.ConvertEmptyToNull(value);
+                if (_prefix != null && !_prefix.EndsWith("/") && !_prefix.EndsWith("\\")) {
+                    _prefix += "/";
+                }
+            }
+        }
+
+        #endregion Public Instance Properties
+    }
+}
