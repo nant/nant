651f3e8f640c9a7f03a3659f1cb519877ff130f3
drieseng <gert.driesen@telenet.be>
2004-10-21 19:40:26 +0000

    added tests for bug #1042996

--- tests/NAnt.DotNet/Types/ResourceFileSetTest.cs
+++ tests/NAnt.DotNet/Types/ResourceFileSetTest.cs
@@ -129,5 +129,68 @@ namespace Tests.NAnt.DotNet.Types {
                 fileset.BaseDirectory.FullName, "file.txt"));
             Assert.AreEqual("file.txt", actualName, "Incorrect manifest resource name.");
         }
+
+        [Test]
+        public void Test_ManifestResourceName_PrefixWithSpecialCharacters() {
+            ResourceFileSet fileset = new ResourceFileSet();
+            fileset.BaseDirectory = TempDirectory;
+            fileset.DynamicPrefix = true;
+
+            string actualName;
+
+            // if part of the prefix starts with a number, it should be
+            // prefixed with an underscore
+            fileset.Prefix = @"Root.1MyProj.Howdy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "file.txt"));
+            Assert.AreEqual("Root._1MyProj.Howdy" + ".mydir.file.txt", actualName,
+                "Incorrect manifest resource name.");
+
+            // any character in the prefix that is neither letter nor digit should
+            // be replaced with an underscore
+            fileset.Prefix = @"Root.-MyProj.H0w!dy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "file.txt"));
+            Assert.AreEqual("Root._MyProj.H0w_dy" + ".mydir.file.txt", actualName,
+                "Incorrect manifest resource name.");
+
+            // the file name part of a manifest resource name can start with a 
+            // digit
+            fileset.Prefix = @"Root.MyProj.Howdy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "1file.txt"));
+            Assert.AreEqual("Root.MyProj.Howdy" + ".mydir.1file.txt", actualName,
+                "Incorrect manifest resource name.");
+
+            // the extension part of a manifest resource name can start with a 
+            // digit
+            fileset.Prefix = @"Root.MyProj.Howdy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "file.1txt"));
+            Assert.AreEqual("Root.MyProj.Howdy" + ".mydir.file.1txt", actualName,
+                "Incorrect manifest resource name.");
+
+            // the file name part of a manifest resource name can contain 
+            // characters that are neither letter nor digit
+            fileset.Prefix = @"Root.MyProj.Howdy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "f-ile.txt"));
+            Assert.AreEqual("Root.MyProj.Howdy" + ".mydir.f-ile.txt", actualName,
+                "Incorrect manifest resource name.");
+
+            // the extension part of a manifest resource name can contain 
+            // characters that are neither letter nor digit
+            fileset.Prefix = @"Root.MyProj.Howdy";
+            actualName = fileset.GetManifestResourceName(Path.Combine(
+                fileset.BaseDirectory.FullName, "mydir" + Path.DirectorySeparatorChar 
+                + "file.t!xt"));
+            Assert.AreEqual("Root.MyProj.Howdy" + ".mydir.file.t!xt", actualName,
+                "Incorrect manifest resource name.");
+        }
     }
 }
