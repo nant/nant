6a2f14aa77705442d6d2ff7a1aa04eeb3f1a64f8
drieseng <gert.driesen@telenet.be>
2005-07-16 11:52:41 +0000

    Added tests for duplicate attribute.

--- tests/NAnt.Compression/Tasks/ZipTaskTest.cs
+++ tests/NAnt.Compression/Tasks/ZipTaskTest.cs
@@ -46,7 +46,7 @@ namespace Tests.NAnt.Compression.Tasks {
                 </project>";
 
             CreateTempDir("src");
-            CreateTempFile("src\\temp1.file","hello");
+            CreateTempFile(Path.Combine("src", "temp1.file"),"hello");
             RunBuild(projectXML);
             Assert.IsTrue(File.Exists(Path.Combine(TempDirName,"test.zip")),
                 "Zip File not created.");
@@ -95,6 +95,172 @@ namespace Tests.NAnt.Compression.Tasks {
                 // error message should contain path of file that does not exist
                 Assert.IsTrue(be.RawMessage.IndexOf("whatever/test.txt") != -1, "#6");
             }
+        }
+
+        [Test]
+        public void Duplicate_Add() {
+            const string projectXML = @"<?xml version='1.0'?>
+                <project>
+                    <zip zipfile='test.zip' duplicate='Add'>
+                        <fileset basedir='folder1' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                        <fileset basedir='folder2' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                    </zip>
+                    <unzip zipfile='test.zip' todir='extract' />
+                </project>";
+
+            CreateTempDir("folder1");
+            CreateTempFile(Path.Combine("folder1", "test.txt"), "folder1");
+            CreateTempDir("folder2");
+            CreateTempFile(Path.Combine("folder2", "test.txt"), "folder2");
+
+            RunBuild(projectXML);
+
+            string extractDir = Path.Combine(TempDirName, "extract");
+            Assert.IsTrue(Directory.Exists(extractDir), "#1");
+            string binDir = Path.Combine(extractDir, "bin");
+            Assert.IsTrue(Directory.Exists(binDir), "#2");
+            string testFile = Path.Combine(binDir, "test.txt");
+            Assert.IsTrue(File.Exists(testFile), "#3");
+
+            // finally check whether second entry (from folder2) was indeed
+            // added to zip file (and as such extracted from it)
+            using (StreamReader sr = new StreamReader(testFile, true)) {
+                Assert.AreEqual("folder2", sr.ReadToEnd(), "#4");
+                sr.Close();
+            }
+        }
+
+        /// <summary>
+        /// Verifies whether a build error is reported if an invalid value is
+        /// specified for the "duplicate" attribute.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(TestBuildException))]
+        public void Duplicate_Invalid() {
+            const string projectXML = @"<?xml version='1.0'?>
+                <project>
+                    <zip zipfile='test.zip' duplicate='invalid'>
+                        <fileset basedir='folder1' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                        <fileset basedir='folder2' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                    </zip>
+                    <unzip zipfile='test.zip' todir='extract' />
+                </project>";
+
+            CreateTempDir("folder1");
+            CreateTempFile(Path.Combine("folder1", "test.txt"), "folder1");
+            CreateTempDir("folder2");
+            CreateTempFile(Path.Combine("folder2", "test.txt"), "folder2");
+
+            RunBuild(projectXML);
+        }
+
+        /// <summary>
+        /// Verifies whether Add is the default value for the "duplicate"
+        /// attribute.
+        /// </summary>
+        [Test]
+        public void Duplicate_Add_Default() {
+            const string projectXML = @"<?xml version='1.0'?>
+                <project>
+                    <zip zipfile='test.zip'>
+                        <fileset basedir='folder1' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                        <fileset basedir='folder2' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                    </zip>
+                    <unzip zipfile='test.zip' todir='extract' />
+                </project>";
+
+            CreateTempDir("folder1");
+            CreateTempFile(Path.Combine("folder1", "test.txt"), "folder1");
+            CreateTempDir("folder2");
+            CreateTempFile(Path.Combine("folder2", "test.txt"), "folder2");
+
+            RunBuild(projectXML);
+
+            string extractDir = Path.Combine(TempDirName, "extract");
+            Assert.IsTrue(Directory.Exists(extractDir), "#1");
+            string binDir = Path.Combine(extractDir, "bin");
+            Assert.IsTrue(Directory.Exists(binDir), "#2");
+            string testFile = Path.Combine(binDir, "test.txt");
+            Assert.IsTrue(File.Exists(testFile), "#3");
+
+            // finally check whether second entry (from folder2) was indeed
+            // added to zip file (and as such extracted from it)
+            using (StreamReader sr = new StreamReader(testFile, true)) {
+                Assert.AreEqual("folder2", sr.ReadToEnd(), "#4");
+                sr.Close();
+            }
+        }
+
+        [Test]
+        public void Duplicate_Preserve() {
+            const string projectXML = @"<?xml version='1.0'?>
+                <project>
+                    <zip zipfile='test.zip' duplicate='Preserve'>
+                        <fileset basedir='folder1' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                        <fileset basedir='folder2' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                    </zip>
+                    <unzip zipfile='test.zip' todir='extract' />
+                </project>";
+
+            CreateTempDir("folder1");
+            CreateTempFile(Path.Combine("folder1", "test.txt"), "folder1");
+            CreateTempDir("folder2");
+            CreateTempFile(Path.Combine("folder2", "test.txt"), "folder2");
+
+            RunBuild(projectXML);
+
+            string extractDir = Path.Combine(TempDirName, "extract");
+            Assert.IsTrue(Directory.Exists(extractDir), "#1");
+            string binDir = Path.Combine(extractDir, "bin");
+            Assert.IsTrue(Directory.Exists(binDir), "#2");
+            string testFile = Path.Combine(binDir, "test.txt");
+            Assert.IsTrue(File.Exists(testFile), "#3");
+
+            // finally check whether second entry (from folder2) was indeed
+            // added to zip file (and as such extracted from it)
+            using (StreamReader sr = new StreamReader(testFile, true)) {
+                Assert.AreEqual("folder1", sr.ReadToEnd(), "#4");
+                sr.Close();
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(TestBuildException))]
+        public void Duplicate_Fail() {
+            const string projectXML = @"<?xml version='1.0'?>
+                <project>
+                    <zip zipfile='test.zip' duplicate='Fail'>
+                        <fileset basedir='folder1' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                        <fileset basedir='folder2' prefix='bin'>
+                            <include name='test.txt' />
+                        </fileset>
+                    </zip>
+                </project>";
+
+            CreateTempDir("folder1");
+            CreateTempFile(Path.Combine("folder1", "test.txt"), "folder1");
+            CreateTempDir("folder2");
+            CreateTempFile(Path.Combine("folder2", "test.txt"), "folder2");
+
+            RunBuild(projectXML);
         }
     }
 }
