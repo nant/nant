a8813e566ec84e589d0bd577225a2b98cb9dfc84
drieseng <gert.driesen@telenet.be>
2006-10-15 12:57:57 +0000

    Use Managed property instead of deprecated UseRuntimeEngine property.

--- src/NAnt.SourceControl/Tasks/AbstractCvsTask.cs
+++ src/NAnt.SourceControl/Tasks/AbstractCvsTask.cs
@@ -463,7 +463,7 @@ namespace NAnt.SourceControl.Tasks {
             }
 
             if (this.UseSharpCvsLib) {
-                UseRuntimeEngine = true;
+                Managed = true;
             }
 
             // Set verbose logging on the #cvslib client if used.
