b0ec41a83ec97b7f817b74524b16560ca69531b7
drieseng <gert.driesen@telenet.be>
2008-04-16 09:48:22 +0000

    Improved tests with deletion of environment variables.

--- tests/NAnt.Core/Tasks/SetEnvTaskTest.cs
+++ tests/NAnt.Core/Tasks/SetEnvTaskTest.cs
@@ -32,38 +32,68 @@ namespace Tests.NAnt.Core.Tasks {
         protected override void SetUp() {
             base.SetUp();
         }
-        
+
         [Test]
-        public void Test_SetSingleEnv() {
-            string _xml = @"
+        public void SetSingleEnv() {
+            string _xml1 = @"
                     <project>
-                        <setenv name='Foo' value='Some Value'/>
+                        <setenv name='FooA' value='ValueA' />
+                        <setenv name='FooB' value='ValueB' />
+                        <setenv name='FooC' value='ValueC' />
                     </project>";
-            RunBuild(_xml);
-            Assert.IsTrue( Environment.GetEnvironmentVariable("Foo") != null, 
-                "Environment variable Foo should have been set" );
-            Assert.IsTrue( Environment.GetEnvironmentVariable("Foo") == "Some Value", 
-                "Environment variable Foo should have been set to 'Some Value'" );
+            RunBuild(_xml1);
+            Assert.AreEqual ("ValueA", Environment.GetEnvironmentVariable("FooA"), "#A1");
+            Assert.AreEqual ("ValueB", Environment.GetEnvironmentVariable("FooB"), "#A2");
+            Assert.AreEqual ("ValueC", Environment.GetEnvironmentVariable("FooC"), "#A3");
+
+            string _xml2 = @"
+                    <project>
+                        <setenv name='FooA' value='' />
+                        <setenv name='FooB' />
+                        <setenv name='FooC' value=' ' />
+                        <setenv name='FooD' value='' />
+                        <setenv name='FooE' />
+                        <setenv name='FooF' value=' ' />
+                    </project>";
+            RunBuild(_xml2);
+            Assert.IsNull (Environment.GetEnvironmentVariable("FooA"), "#B1");
+            Assert.IsNull (Environment.GetEnvironmentVariable("FooB"), "#B2");
+            Assert.AreEqual (" ", Environment.GetEnvironmentVariable("FooC"), "#B3");
+            Assert.IsNull (Environment.GetEnvironmentVariable("FooD"), "#B4");
+            Assert.IsNull (Environment.GetEnvironmentVariable("FooE"), "#B5");
+            Assert.AreEqual (" ", Environment.GetEnvironmentVariable("FooF"), "#B6");
         }
-        
+
         [Test]
         public void Test_SetMultipleEnvVars() {
             string _xml = @"
                     <project>
                         <setenv>
                             <variable name='var1' value='value1' />
-                            <variable name='var2' value='value2' />
+                            <variable name='var2' value='value2' unless='${1==2}' />
+                            <variable name='var3' value='value3' />
+                            <variable name='var4' value='value4' />
+                            <variable name='var5' value='' />
+                            <variable name='var6' />
+                            <variable name='var7' value=' ' />
+                            <variable name='var8' value='value8' unless='${1==1}' />
+                            <variable name='var9' value='value9' if='${1==1}' />
+                        </setenv>
+                        <setenv>
+                            <variable name='var3' value='' />
+                            <variable name='var4' />
                         </setenv>
                     </project>";
             RunBuild(_xml);
-            Assert.IsTrue( Environment.GetEnvironmentVariable("var1") != null, 
-                "Environment variable var1 should have been set" );
-            Assert.IsTrue( Environment.GetEnvironmentVariable("var1") == "value1", 
-                "Environment variable var1 should have been set to 'Some Value'" );
-            Assert.IsTrue( Environment.GetEnvironmentVariable("var2") != null, 
-                "Environment variable var2 should have been set" );
-            Assert.IsTrue( Environment.GetEnvironmentVariable("var2") == "value2", 
-                "Environment variable var2 should have been set to 'value2'" );
+            Assert.AreEqual ("value1", Environment.GetEnvironmentVariable("var1"), "#1");
+            Assert.AreEqual ("value2", Environment.GetEnvironmentVariable("var2"), "#2");
+            Assert.IsNull (Environment.GetEnvironmentVariable("var3"), "#3");
+            Assert.IsNull (Environment.GetEnvironmentVariable("var4"), "#4");
+            Assert.IsNull (Environment.GetEnvironmentVariable("var5"), "#5");
+            Assert.IsNull (Environment.GetEnvironmentVariable("var6"), "#6");
+            Assert.AreEqual (" ", Environment.GetEnvironmentVariable("var7"), "#7");
+            Assert.IsNull (Environment.GetEnvironmentVariable("var8"), "#8");
+            Assert.AreEqual ("value9", Environment.GetEnvironmentVariable("var9"), "#9");
         }
         
         [Test]
@@ -81,6 +111,7 @@ namespace Tests.NAnt.Core.Tasks {
             Assert.IsTrue( Environment.GetEnvironmentVariable("var3") == "value3:value2", 
                 "Environment variable var3 should have been set to 'value3:value2'" );
         }
+
         [Test]
         public void Test_UsePathAttribute() {
             string _xml = @"
@@ -93,6 +124,7 @@ namespace Tests.NAnt.Core.Tasks {
             Assert.IsTrue( Environment.GetEnvironmentVariable("test_path") == "/home/foo", 
                 "Environment variable test_path should have been set to '/home/foo'" );
         }
+
         [Test]
         public void Test_NestedPathElement() {
             string expectedPath = string.Format(@"{0}{1}dir1{2}{0}{1}dir2",
