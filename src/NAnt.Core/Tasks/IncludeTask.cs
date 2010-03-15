e6a8edca0f1001ffbe28713835197d46a697655e
Charles Chan <cchan_qa@users.sourceforge.net>
2010-03-15 02:00:00 +0000

    [On behalf of Ryan Boggs] Patch 2961747. This patch fixes Bug ID: 2824210 by stripping out the include namespaceURI and replace it with the main build file's namespace.

--- src/NAnt.Core/Tasks/IncludeTask.cs
+++ src/NAnt.Core/Tasks/IncludeTask.cs
@@ -163,9 +163,88 @@ namespace NAnt.Core.Tasks {
             _currentBasedir = Path.GetDirectoryName(includedFileName);
             
             try {
+            
+                // This section addresses SF Bug#:2824210..
+                // 
+                // Description of issue:
+                // The root cause to this bug is mismatching NamespaceURIs between the main 
+                // build file and the include file.  There is no place where the include
+                // build file's Namespace is added to the main build files namespace collection.
+                // So if the NamespaceURI of the include file doesn't match the main build file,
+                // then the Project class would not import any of the include file contents.
+                //
+                // Resolution:
+                // The key is to have the Namespaces match between project and include files.
+                // Rather than remove the Namespace checks in the Project class, I decided to
+                // copy the project's namespace into the include file prior to importing into the
+                // main file.  Unfortunately, it is not an easy task.
+                
+                // Create two XmlDocument variables. One to sanitize the include file (doc) 
+                // and one to pass to the project class (loadDoc).
                 XmlDocument doc = new XmlDocument();
-                doc.Load(includedFileName);
-                Project.InitializeProjectDocument(doc);
+                XmlDocument loadDoc = new XmlDocument();
+                
+                // Gets the namespace from the main project.
+                string projectNamespaceURI = Project.Document.DocumentElement.NamespaceURI;
+                
+                // String variable to hold the main build file's namespace.
+                string projectURI = "";
+                
+                // Rather than loading the xml file directly into the XmlDocument, it is loaded
+                // into an XmlTextReader so any NamespaceURI may be stripped out before proceeding
+                // further.
+                XmlTextReader includeXmlReader = new XmlTextReader(includedFileName);
+                
+                // Turn the namespaces off
+                includeXmlReader.Namespaces = false;
+                
+                // Load the contents of the XmlTextReader into the doc XmlDocument
+                doc.Load(includeXmlReader);
+                
+                // Strip the namespace attribute.
+                doc.DocumentElement.Attributes.RemoveNamedItem("xmlns");
+                
+                // Kill the XmlTextReader
+                ((IDisposable)includeXmlReader).Dispose();
+                includeXmlReader = null;
+                
+                // Assigns the main build file's namespace to the
+                // local string variable if it is not blank.
+                if (!StringUtils.IsNullOrEmpty(projectNamespaceURI)) {
+                    projectURI = projectNamespaceURI;
+                    
+                }
+                
+                // If the projectURI is not empty at this point, add
+                // the Namespace attribute to the doc XmlDocument.
+                if (!StringUtils.IsNullOrEmpty(projectURI)) {
+                    XmlAttribute projAttr = doc.CreateAttribute("xmlns");
+                    projAttr.Value = projectURI;
+                    doc.DocumentElement.Attributes.Append(projAttr);
+                }
+                
+                // Set up a stringwriter and XmlTextWriter variable to pass the
+                // contents of the doc XmlDocument variable to.
+                using (StringWriter includeFileSW = new StringWriter()) {
+                    XmlTextWriter includeFileXW = new XmlTextWriter(includeFileSW);
+                    
+                    // Loads the contents from doc to the XmlTextWriter.
+                    doc.WriteTo(includeFileXW);
+                    
+                    // Then the contents of the XmlTextWriter to the loadDoc XmlDocument
+                    // var.  This will ensure that the main build file's Namespace
+                    // is loaded into the include file before it's passed to the Project
+                    // class.
+                    loadDoc.LoadXml(includeFileSW.ToString());
+                    
+                    // Kill the XmlTextWriter
+                    ((IDisposable)includeFileXW).Dispose();
+                    includeFileXW = null;
+                }
+                
+                // Pass the loadDoc XmlDocument to the project.
+                Project.InitializeProjectDocument(loadDoc);
+                
             } catch (BuildException) {
                 // rethrow exception
                 throw;
--- tests/NAnt.Core/Tasks/IncludeTaskTest.cs
+++ tests/NAnt.Core/Tasks/IncludeTaskTest.cs
@@ -95,7 +95,52 @@ namespace Tests.NAnt.Core.Tasks {
                     "Build exception should have been because of a recursive include." + Environment.NewLine + e.ToString());
             }
         }
-
+        
+        [Test]
+        public void Test_BuildXmlns() {
+            const string buildXmlnsFile = @"<?xml version='1.0'?>
+                        <project default='includeXmlnsTarget' basedir='{0}' xmlns='http://nant.sf.net/release/0.85/nant.xsd'>
+                          <include buildfile='example1.xml' />
+                        </project>";
+                        
+            const string includeNonXmlnsFile = @"<?xml version='1.0'?>
+                        <project>
+                        <target name='includeXmlnsTarget'>
+                          <echo message='Target executed'/>
+                        </target>
+                        </project>";
+                        
+            
+            string includeNonXmlnsFileName = Path.Combine(TempDirName, "example1.xml");
+            TempFile.CreateWithContents(includeNonXmlnsFile, includeNonXmlnsFileName);
+            
+            string result = RunBuild(FormatBuildFile(buildXmlnsFile));
+            Assert.IsTrue(result.IndexOf("Target executed") != -1, "Include target without xmlns did not execute." + Environment.NewLine + result);
+            
+        }
+        
+        [Test]
+        public void Test_IncludeXmlns() {
+            const string buildNonXmlnsFile = @"<?xml version='1.0'?>
+                        <project default='includeXmlnsTarget' basedir='{0}'>
+                          <include buildfile='example2.xml' />
+                        </project>";
+                        
+            const string includeXmlnsFile = @"<?xml version='1.0'?>
+                        <project xmlns='http://nant.sf.net/release/0.85/nant.xsd'>
+                        <target name='includeXmlnsTarget'>
+                          <echo message='Target executed'/>
+                        </target>
+                        </project>";
+                        
+            
+            string includeXmlnsFileName = Path.Combine(TempDirName, "example2.xml");
+            TempFile.CreateWithContents(includeXmlnsFile, includeXmlnsFileName);
+            
+            string result = RunBuild(FormatBuildFile(buildNonXmlnsFile));
+            Assert.IsTrue(result.IndexOf("Target executed") != -1, "Include target with xmlns did not execute." + Environment.NewLine + result);
+            
+        }
 
         private string FormatBuildFile(string format) {
             return String.Format(CultureInfo.InvariantCulture, format, TempDirName);
