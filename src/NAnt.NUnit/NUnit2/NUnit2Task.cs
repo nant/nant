f4d049a82c4b80183817fa7265ed753be304f92a
Ryan Boggs <rmboggs@gmail.com>
2012-01-25 20:46:47 -0800

    Switch back to using XslTransform just in NUnit2Task instead of XslCompiledTransform as the later doesn't print nunit output to the console on .NET.

--- src/NAnt.NUnit/NUnit2/NUnit2Task.cs
+++ src/NAnt.NUnit/NUnit2/NUnit2Task.cs
@@ -435,7 +435,9 @@ namespace NAnt.NUnit2.Tasks {
 
         private void CreateSummaryDocument(string resultFile, TextWriter writer, NUnit2Test test) {
             XPathDocument originalXPathDocument = new XPathDocument(resultFile);
-            XslCompiledTransform summaryXslTransform = new XslCompiledTransform();
+            // Using XslTransform instead of XslCompiledTransform because the later does not
+            // display nunit output for reasons unknown.
+            XslTransform summaryXslTransform = new XslTransform();
             XmlTextReader transformReader = GetTransformReader(test);
             summaryXslTransform.Load(transformReader);
             summaryXslTransform.Transform(originalXPathDocument, null, writer);
@@ -444,7 +446,6 @@ namespace NAnt.NUnit2.Tasks {
         private XmlTextReader GetTransformReader(NUnit2Test test) {
             XmlTextReader transformReader;
             if (test.XsltFile == null) {
-                // TODO: Re-evaluate this process as NUnit will probably not contain the "NUnit.Util.Transform" resource after 2.5.
                 Assembly assembly = Assembly.GetAssembly(typeof(XmlResultWriter));
                 ResourceManager resourceManager = new ResourceManager("NUnit.Util.Transform", assembly);
                 string xmlData = (string) resourceManager.GetObject("Summary.xslt", CultureInfo.InvariantCulture);
