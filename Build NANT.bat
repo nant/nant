c6a8c7e2852c050cd782c60081080b54916964f2
SRH\miles.davies <miles.davies@horiba.com>
2018-11-22 13:49:06 +0000

    returned to defaults for building

--- Build NANT.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-
-nmake -f Makefile.nmake clean bootstrap
-.\bootstrap\nant.exe -t:net-3.5 -buildfile:nant.build -D:project.version=0.94 -D:project.release.type=alpha -D:target.platform=x86 clean release build test
